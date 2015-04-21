66cb15be57940dfbb788054023c37fbfa8ebfc34
Benedikt Hegner <hegner@cern.ch>
2015-04-21 11:04:13 +0200

    adding std::array support

--- python/albers_class_generator.py
+++ python/albers_class_generator.py
@@ -41,9 +41,20 @@ class ClassGenerator(object):
     self.created_classes = []
     self.requested_classes = []
 
+  @staticmethod
+  def check_class(klass):
+    # check whether it is an array
+    if "[" in klass:
+      theType = klass.split("[")[0]
+      number  = klass.split("[")[1].split("]")[0]
+      # transform it into std::array
+      klass = "std::array<%s,%s>" %(theType, number);
+    return klass
+
+
   def load_data_definitions(self):
     stream = open(self.yamlfile, "r")
-    content = yaml.load(stream)      
+    content = yaml.load(stream)
     datatypes = {}
     if content.has_key("datatypes"):
       for klassname, value in content["datatypes"].iteritems():
@@ -55,11 +66,11 @@ class ClassGenerator(object):
           if value.has_key(category):
             for definition in value[category]:
               klass, name = definition.split()[:2]
+              klass = self.check_class(klass)
               comment = definition.split("//")[1]
               definitions.append({"name": name, "type": klass, "description" : comment})
             datatype[category] = definitions
         datatypes[klassname] = datatype
-    print datatypes
     self.datatypes = datatypes
 
   def process(self):
@@ -150,6 +161,9 @@ class ClassGenerator(object):
       elif klass in self.requested_classes:
     #    includes += '#include "%s/%s.h"\n' %(self.package_name,klass)
         includes += '#include "%s.h"\n' %(klass)
+      elif "std::array" in klass:
+          includes += "#include <array>\n"
+          self.created_classes.append(klass)
       else:
         raise Exception("'%s' defines a member of a type '%s' that is not (yet) declared!" %(classname, klass))
     membersCode = ""
@@ -203,6 +217,8 @@ class ClassGenerator(object):
       elif klass in self.requested_classes:
         #includes += '#include "%s/%s.h"\n' %(self.package_name,klass)
         includes += '#include "%s.h"\n' %(klass)
+      elif "std::array" in klass:
+        includes += "#include <array>\n"
       else:
         raise Exception("'%s' defines a member of a type '%s' that is not declared!" %(classname, klass))
 
@@ -216,6 +232,8 @@ class ClassGenerator(object):
         klass = item["type"]
         if klass in self.requested_classes:
           includes += '#include "%s.h"\n' %klass
+        elif "std::array" in klass:
+          includes += "#include <array>\n"
         else:
           raise Exception("'%s' declares a non-allowed many-relation to '%s'!" %(classname, klass))
 
