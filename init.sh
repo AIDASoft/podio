050cd9a85c997f6e6d19a44e82cf94dc6e736f50
Anna <anna.zaborowska@cern.ch>
2015-10-16 09:34:47 +0200

    Missing include of gtest added

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/2.8.9/Linux-i386/bin:${PATH}
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6/setup.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.02.08/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+export CMAKE_PREFIX_PATH=/afs/cern.ch/sw/lcg/releases/gtest/1.7.0-4f83b/x86_64-slc6-gcc48-opt/
 
 export PODIOHOME=$PWD/install
 export LD_LIBRARY_PATH=$PODIO/examples:$PODIOHOME/lib:$LD_LIBRARY_PATH
