3a0d4db8e45a1a4d5c2ec33b15b4c7cecf1aa616
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-01-18 10:22:21 +0100

    Fixing path for python environment

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/2.8.9/Linux-i386/bin:${PATH}
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
 source /afs/cern.ch/exp/fcc/sw/0.5/LCG_80/ROOT/6.04.06/x86_64-slc6-gcc49-opt/bin/thisroot.sh
-source /afs/cern.ch/sw/lcg/releases/LCG_80/Python/2.7.10/x86_64-slc6-gcc49-opt/Python-env.sh
+source /afs/cern.ch/sw/lcg/releases/LCG_80/Python/2.7.9.p1/x86_64-slc6-gcc49-opt/Python-env.sh
 source /afs/cern.ch/sw/lcg/releases/LCG_82/pytools/1.9_python2.7/x86_64-slc6-gcc49-opt/pytools-env.sh
 export CMAKE_PREFIX_PATH=/afs/cern.ch/sw/lcg/releases/gtest/1.7.0-4f83b/x86_64-slc6-gcc49-opt/
 
