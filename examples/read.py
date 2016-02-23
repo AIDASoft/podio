5718555c3d89ee9f77afb2b54229d11c00357249
Colin <colin.bernet@cern.ch>
2016-02-23 16:28:01 +0100

    added python read example

---
+++ examples/read.py
@@ -0,0 +1,16 @@
+import unittest
+from EventStore import EventStore
+
+if __name__ == '__main__':
+    
+    filename = 'example.root'
+    store = EventStore([filename])
+    for i, event in enumerate(store):
+        if i%1000 == 0:
+            print 'reading event', i
+        evinfo = store.get("info")[0]
+        clusters = store.get("clusters")
+        for cluster in clusters:
+            for ihit in range(cluster.Hits_size()):
+                hit = cluster.Hits(ihit)
+                print ' Referenced hit has an energy of', hit.energy()
