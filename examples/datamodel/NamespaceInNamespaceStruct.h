952a89214f6340ff564cf7b1a0840b6f3284ac3b
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-03-09 09:54:38 +0100

    Add missing class from example datamodel.

---
+++ examples/datamodel/NamespaceInNamespaceStruct.h
@@ -0,0 +1,13 @@
+#ifndef NamespaceInNamespaceStruct_H
+#define NamespaceInNamespaceStruct_H
+#include "NamespaceStruct.h"
+
+
+namespace ex2 {
+class NamespaceInNamespaceStruct {
+public:
+ ::ex2::NamespaceStruct data;
+
+};
+} // namespace ex2
+#endif
