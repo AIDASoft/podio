a4803146f8b254f1f35ba5c331c51ffccb88794d
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-10-27 16:35:49 +0100

    Use CMakePackageConfigHelpers fully
    Migrate template podioConfig.cmake.in file to use
    CMakePackageConfigHelpers functionality fully.
    
    Use GNUInstallDirs as templated paths and include exported targets file.
    
    Add custom imported target for podio_class_generator.py script. This
    allows clients of podio to use this in add_custom_command/target
    to generate classes from their yaml files.
    
    TODOS:
    1. podio_class_generator.py will fail in new install layout due to
    mismatch in install locations of script and podio templates. Review how
    to make these paths consistent and relocatable.
    2. a wrapper "podio_class_generate" Cmake function could be supplied.
    One known issue is how to write a add_custom_command/add_custom_target
    pair with OUTPUTS because podio's list of output files is now known
    a priori. One putative idea is to add an additional
    
    `--list-output-files <datamodel.yaml>`
    
    argument to podio_class_generator.py that could output this prior
    to running the genration step. To be reviewed.

--- cmake/podioCreateConfig.cmake
+++ cmake/podioCreateConfig.cmake
@@ -1,9 +1,32 @@
+#--- CMake Config Files -----------------------------------------------
+# - Use CMake's module to help generating relocatable config files
 include(CMakePackageConfigHelpers)
-configure_file(cmake/podioConfig.cmake.in "${PROJECT_BINARY_DIR}/podioConfig.cmake" @ONLY)
-write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/podioConfigVersion.cmake
-                                 VERSION ${podio_VERSION}
-                                 COMPATIBILITY SameMajorVersion )
 
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/podioConfig.cmake
-              ${CMAKE_CURRENT_BINARY_DIR}/podioConfigVersion.cmake
-              DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake )
+# - Versioning
+write_basic_package_version_file(
+  ${CMAKE_CURRENT_BINARY_DIR}/podioConfigVersion.cmake
+  VERSION ${podio_VERSION}
+  COMPATIBILITY SameMajorVersion)
+
+# - Install time config and target files
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/podioConfig.cmake.in
+  "${PROJECT_BINARY_DIR}/podioConfig.cmake"
+  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/podio"
+  PATH_VARS
+    CMAKE_INSTALL_BINDIR
+    CMAKE_INSTALL_INCLUDEDIR
+    CMAKE_INSTALL_LIBDIR
+  )
+
+# - install and export
+install(FILES
+  "${PROJECT_BINARY_DIR}/podioConfigVersion.cmake"
+  "${PROJECT_BINARY_DIR}/podioConfig.cmake"
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/podio"
+  )
+install(EXPORT podioTargets
+  NAMESPACE podio::
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/podio"
+  )
+
+
