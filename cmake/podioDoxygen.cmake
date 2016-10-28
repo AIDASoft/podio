0b0973f3d3a27a408322cbe0ca376d2f3e300d62
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-10-28 12:34:50 +0100

    Build docs by default and as part of ALL
    Default podio_documentation option to ON so that docs are built by
    default. Update build of docs to use split add_custom_command/target
    pair to simplify generation and express dependencies properly:
    
    - Docs should be rebuilt when library is
    - Docs should be rebuilt when inputs to Doxygen change
    
    Make doxygen logging QUIET so only true warnings/errors are shown
    in build tool output as for compiling.

--- cmake/podioDoxygen.cmake
+++ cmake/podioDoxygen.cmake
@@ -3,11 +3,22 @@ if(DOXYGEN_FOUND)
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/doc/Doxyfile.in
                  ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/doc/doxy-boot.js.in
-                 ${CMAKE_BINARY_DIR}/doxygen/html/doxy-boot.js)
-  add_custom_target(doc
-                    ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-                    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-                    COMMENT "Generating API documentation with Doxygen" VERBATIM)
+    ${PROJECT_BINARY_DIR}/doxygen/html/doxy-boot.js)
+
+  # - custom command
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/doxygen/html/index.html
+    COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    DEPENDS podio ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile ${CMAKE_CURRENT_BINARY_DIR}/doxygen/html/doxy-boot.js
+    COMMENT "Generating API documentation with Doxygen"
+    VERBATIM)
+
+
+  add_custom_target(doc ALL DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/doxygen/html/index.html)
+
+    #    ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+    #               WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    #               COMMENT "Generating API documentation with Doxygen" VERBATIM)
 
   install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doxygen DESTINATION ${CMAKE_INSTALL_DOCDIR} OPTIONAL)
 endif()
