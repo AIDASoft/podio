d7256ce0720f8debdf1ae8756788db69983a76f3
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-12-09 16:34:09 +0100

    Move to LCG_87

--- init.sh
+++ init.sh
@@ -9,9 +9,9 @@ if [[ "$unamestr" == 'Linux' ]]; then
     platform='Linux'
     if [[ -d /afs/cern.ch/sw/lcg ]] && [[ `dnsdomainname` = 'cern.ch' ]] ; then
 	#should check domain to make sure we're at CERN
-	#or is this software available somewhere in Lyon? 
+	#or is this software available somewhere in Lyon?
 	sw_afs=1
-    source /afs/cern.ch/sw/lcg/views/LCG_83/x86_64-slc6-gcc49-opt/setup.sh
+    source /afs/cern.ch/sw/lcg/views/LCG_87/x86_64-slc6-gcc49-opt/setup.sh
 	echo cmake and root taken from /afs/cern.ch/sw/lcg
     fi
     export LD_LIBRARY_PATH=$PODIO/tests:$PODIO/lib:$PODIO/tests:$LD_LIBRARY_PATH
