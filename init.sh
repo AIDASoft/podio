bcbbf71088b76f1be6e652303deaa7b7ed460ecf
Joschka Lingemann <joschka.lingemann@cern.ch>
2017-02-21 10:49:44 +0100

    Remove unused variable, add check if PODIO is set.

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
-
 platform='unknown'
-sw_afs=0
 unamestr=`uname`
 
-export PODIO=$PWD/install
+if [ -z "$FCCEDM" ]; then
+    export PODIO=$PWD/install
+    echo "PODIO is unset, setting to $PODIO"
+fi
 
 if [[ "$unamestr" == 'Linux' ]]; then
     platform='Linux'
     if [[ -d /cvmfs/sft.cern.ch/lcg ]]; then
 	#should check domain to make sure we're at CERN
 	#or is this software available somewhere in Lyon?
-	sw_afs=1
     source /cvmfs/sft.cern.ch/lcg/views/LCG_87/x86_64-slc6-gcc49-opt/setup.sh
 	echo cmake and root taken from /cvmfs/sft.cern.ch/lcg
     fi
