ca05c32b6fe92714acb0d9fd7297241e14bfdd58
Joschka Lingemann <joschka.lingemann@cern.ch>
2017-02-21 10:12:04 +0100

    Move to cvmfs

--- init.sh
+++ init.sh
@@ -7,12 +7,12 @@ export PODIO=$PWD/install
 
 if [[ "$unamestr" == 'Linux' ]]; then
     platform='Linux'
-    if [[ -d /afs/cern.ch/sw/lcg ]] && [[ `dnsdomainname` = 'cern.ch' ]] ; then
+    if [[ -d /cvmfs/sft.cern.ch/lcg ]]; then
 	#should check domain to make sure we're at CERN
 	#or is this software available somewhere in Lyon?
 	sw_afs=1
-    source /afs/cern.ch/sw/lcg/views/LCG_87/x86_64-slc6-gcc49-opt/setup.sh
-	echo cmake and root taken from /afs/cern.ch/sw/lcg
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_87/x86_64-slc6-gcc49-opt/setup.sh
+	echo cmake and root taken from /cvmfs/sft.cern.ch/lcg
     fi
     export LD_LIBRARY_PATH=$PODIO/tests:$PODIO/lib:$PODIO/tests:$LD_LIBRARY_PATH
 elif [[ "$unamestr" == 'Darwin' ]]; then
