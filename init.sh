6955207b002d5d3d90f59b19bd9ae5eeb10cb021
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2018-10-18 17:37:19 +0200

    Define PODIO env variable

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 
 # source FCC externals
 source /cvmfs/fcc.cern.ch/sw/views/releases/externals/93.0.0/x86_64-$OS-gcc62-opt/setup.sh
+
+# Define path to podio
+# CMakeLists.txt relies on some environment variables
+export PODIO=$FCCVIEW
