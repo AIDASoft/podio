0073ea630e6b9b30d880f8d8c32514d7c0f58529
shrey183 <shrey183@gmail.com>
2019-07-03 00:31:41 +0200

    found dimensions, however there is a bug.
    - Dimensions can be found from the dataset.
    - Segfault occurs (at Line 83) while trying to copy elements from the buffer to info_1. I tried to debug this and I think that the issue is that we have not allocated memory for m_data. But then if we malloc some pointer with the right size and trying setting address to that pointer, the segfault still occurs. I will try to get a deeper understanding of EventInfoCollection to debug this error.

--- tests/read_dummy.cpp
+++ tests/read_dummy.cpp
@@ -5,8 +5,7 @@
 // STL
 #include <iostream>
 #include <vector>
-#include <memory>
-
+#include <cstring>
 // podio specific includes
 #include "podio/EventStore.h"
 
@@ -21,6 +20,9 @@ const H5std_string DATASET_NAME_2( "EventInfo_data_2" );
 const H5std_string EventInfo_Number( "Number" );
 const int RANK = 1;
 
+
+
+
 int main()
 {
 
@@ -30,56 +32,62 @@ int main()
 		mtype_EventInfo.insertMember(EventInfo_Number,HOFFSET(EventInfoData, Number), PredType::NATIVE_INT);
 
 		// open file in read only mode
-		std::shared_ptr<H5File> file(new H5File(FILE_NAME, H5F_ACC_RDONLY));
+		H5File file(FILE_NAME, H5F_ACC_RDONLY);
 	
 		// DATASET 1
-		std::shared_ptr<DataSet> dataset1(new DataSet(file->openDataSet(DATASET_NAME_1)));
+		DataSet dataset1 = file.openDataSet(DATASET_NAME_1);
 		
 		// DATASET 2
-		std::shared_ptr<DataSet> dataset2(new DataSet(file->openDataSet(DATASET_NAME_2)));
+		DataSet dataset2 = file.openDataSet(DATASET_NAME_2);
 	
-		// Buffer to store data for Dataset 1 
-		EventInfoData data_1[2]; 
-		
-		EventInfoData data_2[4];
+		// Extract information from the datasets
 
+		DataSpace dataspace1 = dataset1.getSpace();
+		DataSpace dataspace2 = dataset2.getSpace();
+
+		hsize_t dim_1[2];
+		hsize_t dim_2[2];
+
+		dataspace1.getSimpleExtentDims(dim_1, NULL);
+		dataspace2.getSimpleExtentDims(dim_2, NULL);
+
+		std::cout<<"dimension of dataset 1 = "<< (unsigned long)dim_1[0]<<std::endl;
+	        std::cout<<"dimension of dataset 2 = "<<(unsigned long)dim_2[0]<<std::endl;
+		
+		hsize_t size_1 = dataset1.getStorageSize();
+		hsize_t size_2 = dataset2.getStorageSize();
+
+		std::cout<<"storage space for dataset 1"<<(unsigned long)size_1<<std::endl;
+		std::cout<<"storage space for dataset 2"<<(unsigned long)size_2<<std::endl; 
+		
 		// Read data to file
 		std::cout<<"Reading data..."<<std::endl;
-
-		dataset1->read(data_1, mtype_EventInfo);	
-		dataset2->read(data_2, mtype_EventInfo);
-					
-		// recover the EventInfoCollection
+		
 		auto store = podio::EventStore();
 		auto& info_1 = store.create<EventInfoCollection>("info_1");
-		auto& info_2 = store.create<EventInfoCollection>("info_2");
+		
+		void* buffer_1 = malloc(size_1);	
+		dataset1.read(buffer_1, mtype_EventInfo);	
+		
+		//check if we managed to read the stuff into buffer_1
+		EventInfoData* p = reinterpret_cast<EventInfoData*>(buffer_1);
+		std::cout<<p[0].Number<<std::endl;
+		std::cout<<p[1].Number<<std::endl;
+				
 
-		for(int i=0; i<2; i++)
-		{
-			auto temp = EventInfo();
-			temp.Number(data_1[i].Number);
-			info_1.push_back(temp);
-		}
-	        	
-		for(int i=0; i<4; i++)
-		{
-			auto temp = EventInfo();
-			temp.Number(data_2[i].Number);
-			info_2.push_back(temp);
-		}
+			
+		void* dest = info_1->_getBuffer();
 
+		memcpy(dest, buffer_1,size_1);
 
+		// SEG FAULT OCCURS HERE when creating EventInfoObj
+		info_1->prepareAfterRead();  
+		
 		// print data to verify
 		std::cout<<"Dataset I"<<std::endl;
-		
-		for(int i=0; i<2; i++)	
-			std::cout<<info_1[i]<<std::endl; 
-			
-		std::cout<<"Dataset II"<<std::endl;
+		std::cout<<info_1<<std::endl;
 
-		for(int i=0; i<4; i++)	
-			std::cout<<info_2[i]<<std::endl;
-			
+		store.clearCollections();		
 
 	} // end of try block
 	
