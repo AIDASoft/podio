68d3fa1d31d29180e3547b19e4906500536953f3
shrey183 <shrey183@gmail.com>
2019-07-30 22:09:54 +0200

    demo with writer and register HDF5

--- tests/write_dummy.cpp
+++ tests/write_dummy.cpp
@@ -11,17 +11,20 @@
 // podio specific includes
 #include "podio/EventStore.h"
 #include "podio/CollectionBase.h"
+#include "podio/WriterRegister.h"
+#include "podio/HDF5Writer.h"
 
 // HDF5 specific includes
 #include "H5Cpp.h"
 #include "H5_EventInfoData.h"
-#include "podio/HDF5Writer.h"
 
-using namespace H5;	
-using namespace std; 
+
+using namespace H5;
+using namespace std;
+using namespace podio;
 
 const H5std_string FILE_NAME("dummy.h5");
-const H5std_string FILE_NAME_1("dummy_writer.h5");
+const H5std_string FILE_NAME_1("dummy_new.h5");
 
 map<const type_info*, string> m;
 
@@ -29,39 +32,39 @@ int main()
 {
 
 	try{
-		// create file
-		H5File file(FILE_NAME, H5F_ACC_TRUNC);
-		
-		// create EventStore
-		auto store = podio::EventStore();
-		auto writer = podio::HDF5Writer(FILE_NAME_1, &store);
-		
-		auto& info = store.create<EventInfoCollection>("info");
-		
-		// for writing EventInfoData	
-		// H5_EventInfoData h5eid; 	
-		
-		auto h5eid = H5_EventInfoData::getInstance();
 
-			
+		// create EventStore
+		auto store = EventStore();
+
+		// declare HDF5Writer
+		auto writer = HDF5Writer(FILE_NAME_1, &store);
+
+		// declare register
+		WriterRegister* writer_register = WriterRegister::getInstance();
+		// we should register EventInfoData writer
+		H5_EventInfoData* eid = H5_EventInfoData::getInstance();
+		writer_register->register_writer(eid);
+
+		auto& info = store.create<EventInfoCollection>("info");
 		writer.registerForWrite<EventInfoCollection>("info");
 
+
 		unsigned nevents = 3;
-		
-		for(unsigned i=0; i<nevents; ++i) 
+
+		for(unsigned i=0; i<nevents; ++i)
 		{
 			std::cout << "processing event " << i << std::endl;
 
 			// we just write the same stuff nevents' times.
-			
+
 			auto item_1 = EventInfo();
-			item_1.Number(20); 
+			item_1.Number(20);
 
 			auto item_2 = EventInfo();
 			item_2.Number(21);
 
 			auto item_3 = EventInfo();
-			item_3.Number(22); 
+			item_3.Number(22);
 
 			auto item_4 = EventInfo();
 			item_4.Number(23);
@@ -70,28 +73,23 @@ int main()
 			info.push_back(item_2);
 			info.push_back(item_3);
 			info.push_back(item_4);
-			
-			h5eid->writeH5(file, info);
-			
-			writer.writeEvent();
 
+			writer.writeEvent(writer_register->h5map);
 			store.clearCollections();
 
-		
 		}
-	
-	
+
 
 	} // end of try block
-	
-	
+
+
 	// catch failure caused by the H5File operations
 	catch( FileIException error )
 	{
 		error.printErrorStack();
 		return -1;
 	}
-	
+
 	// catch failure caused by the DataSet operations
 	catch( DataSetIException error )
 	{
@@ -109,7 +107,7 @@ int main()
 		std::cout<<"Something terrible happened!"<<std::endl;
 		return -1;
 	}
-	
+
 
 	return 0;
 }
