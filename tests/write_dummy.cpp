708f6526097c008b9e5ad6c8c265dc4cdff557fe
shrey183 <shrey183@gmail.com>
2019-08-15 21:27:59 +0200

    with ExampleMC

--- tests/write_dummy.cpp
+++ tests/write_dummy.cpp
@@ -11,7 +11,24 @@
 
 // HDF5 specific includes
 #include "H5Cpp.h"
-#include "H5_Writer_EventInfoCollection.h"
+
+
+#include "H5EventInfoCollection.h"
+#include "H5ExampleMCCollection.h"
+#include "H5ExampleHitCollection.h"
+///*
+#include "H5ExampleClusterCollection.h"
+#include "H5ExampleReferencingTypeCollection.h"
+#include "H5ExampleWithComponentCollection.h"
+//#include "H5ExampleWithOneRelationCollection.h"
+#include "H5ExampleWithVectorMemberCollection.h"
+#include "H5ExampleWithNamespaceCollection.h"
+#include "H5ExampleWithARelationCollection.h"
+#include "H5ExampleWithARelationCollection.h"
+//*/
+
+#include "H5ExampleWithStringCollection.h"
+#include "H5ExampleWithArrayCollection.h"
 
 using namespace H5;
 using namespace std;
@@ -30,33 +47,112 @@ int main()
 		// declare HDF5Writer
 		auto writer = HDF5Writer(FILE_NAME, &store);
 
-		// create EventInfoCollection
-		auto& info = store.create<EventInfoCollection>("info");
 
-		writer.registerForWrite<H5_Writer_EventInfoCollection>("info");
+		auto& info       = store.create<EventInfoCollection>("info");
+		auto& mcps       = store.create<ExampleMCCollection>("mcparticles");
 
-		unsigned nevents = 3;
+		/*
+		auto& hits       = store.create<ExampleHitCollection>("hits");
+		auto& clusters   = store.create<ExampleClusterCollection>("clusters");
+		auto& refs       = store.create<ExampleReferencingTypeCollection>("refs");
+		auto& refs2      = store.create<ExampleReferencingTypeCollection>("refs2");
+		auto& comps      = store.create<ExampleWithComponentCollection>("Component");
+		//auto& oneRels    = store.create<ExampleWithOneRelationCollection>("OneRelation");
+		auto& vecs       = store.create<ExampleWithVectorMemberCollection>("WithVectorMember");
+		auto& namesps    = store.create<ex::ExampleWithNamespaceCollection>("WithNamespaceMember");
+		auto& namesprels = store.create<ex::ExampleWithARelationCollection>("WithNamespaceRelation");
+		auto& cpytest    = store.create<ex::ExampleWithARelationCollection>("WithNamespaceRelationCopy");
+		auto& strings    = store.create<ExampleWithStringCollection>("strings");
+		auto& arrays     = store.create<ExampleWithArrayCollection>("arrays");
+		*/
+
+		writer.registerForWrite<H5EventInfoCollection>("info");
+	    writer.registerForWrite<H5ExampleMCCollection>("mcparticles");
+
+		/*
+	    writer.registerForWrite<H5ExampleHitCollection>("hits");
+		writer.registerForWrite<H5ExampleClusterCollection>("clusters");
+	    writer.registerForWrite<H5ExampleReferencingTypeCollection>("refs");
+	    writer.registerForWrite<H5ExampleReferencingTypeCollection>("refs2");
+	    writer.registerForWrite<H5ExampleWithComponentCollection>("Component");
+	    //writer.registerForWrite<H5ExampleWithOneRelationCollection>("OneRelation");
+	    writer.registerForWrite<H5ExampleWithVectorMemberCollection>("WithVectorMember");
+	    writer.registerForWrite<H5ExampleWithNamespaceCollection>("WithNamespaceMember");
+	    writer.registerForWrite<H5ExampleWithARelationCollection>("WithNamespaceRelation");
+	    writer.registerForWrite<H5ExampleWithARelationCollection>("WithNamespaceRelationCopy");
+		writer.registerForWrite<H5ExampleWithStringCollection>("strings");
+		writer.registerForWrite<H5ExampleWithArrayCollection>("arrays");
+		*/
+
+		unsigned nevents = 2;
 
 		for(unsigned i=0; i<nevents; ++i)
 		{
 			std::cout << "processing event " << i << std::endl;
 
-			auto item_1 = EventInfo();
-			item_1.Number(20);
+			auto item1 = EventInfo();
+			item1.Number(i);
+			info.push_back(item1);
 
-			auto item_2 = EventInfo();
-			item_2.Number(21);
+			// ---- add some MC particles ----
+			auto mcp0 = ExampleMC();
+			auto mcp1 = ExampleMC();
+			auto mcp2 = ExampleMC();
+			auto mcp3 = ExampleMC();
+			auto mcp4 = ExampleMC();
+			auto mcp5 = ExampleMC();
+			auto mcp6 = ExampleMC();
+			auto mcp7 = ExampleMC();
+			auto mcp8 = ExampleMC();
+			auto mcp9 = ExampleMC();
 
-			auto item_3 = EventInfo();
-			item_3.Number(22);
+			mcps.push_back( mcp0 ) ;
+			mcps.push_back( mcp1 ) ;
+			mcps.push_back( mcp2 ) ;
+			mcps.push_back( mcp3 ) ;
+			mcps.push_back( mcp4 ) ;
+			mcps.push_back( mcp5 ) ;
+			mcps.push_back( mcp6 ) ;
+			mcps.push_back( mcp7 ) ;
+			mcps.push_back( mcp8 ) ;
+			mcps.push_back( mcp9 ) ;
 
-			auto item_4 = EventInfo();
-			item_4.Number(23);
+			// --- add some daughter relations
+			auto p = ExampleMC();
+			auto d = ExampleMC();
 
-			info.push_back(item_1);
-			info.push_back(item_2);
-			info.push_back(item_3);
-			info.push_back(item_4);
+			p = mcps[0] ;
+			p.adddaughters( mcps[2] ) ;
+			p.adddaughters( mcps[3] ) ;
+			p.adddaughters( mcps[4] ) ;
+			p.adddaughters( mcps[5] ) ;
+			p = mcps[1] ;
+			p.adddaughters( mcps[2] ) ;
+			p.adddaughters( mcps[3] ) ;
+			p.adddaughters( mcps[4] ) ;
+			p.adddaughters( mcps[5] ) ;
+			p = mcps[2] ;
+			p.adddaughters( mcps[6] ) ;
+			p.adddaughters( mcps[7] ) ;
+			p.adddaughters( mcps[8] ) ;
+			p.adddaughters( mcps[9] ) ;
+			p = mcps[3] ;
+			p.adddaughters( mcps[6] ) ;
+			p.adddaughters( mcps[7] ) ;
+			p.adddaughters( mcps[8] ) ;
+			p.adddaughters( mcps[9] ) ;
+
+			//--- now fix the parent relations
+			for( unsigned j=0,N=mcps.size();j<N;++j){
+			p = mcps[j] ;
+			for(auto it = p.daughters_begin(), end = p.daughters_end() ; it!=end ; ++it ){
+			int dIndex = it->getObjectID().index ;
+			d = mcps[ dIndex ] ;
+			d.addparents( p ) ;
+			}
+			}
+
+
 
 			writer.writeEvent();
 			store.clearCollections();
