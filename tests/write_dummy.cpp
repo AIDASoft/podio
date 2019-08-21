c7957c888a6a5723282a3fa93710748a8203d5b4
shrey183 <shrey183@gmail.com>
2019-08-21 10:10:10 +0200

    write_dummy for directories

--- tests/write_dummy.cpp
+++ tests/write_dummy.cpp
@@ -15,8 +15,8 @@
 
 #include "H5EventInfoCollection.h"
 #include "H5ExampleMCCollection.h"
+/*
 #include "H5ExampleHitCollection.h"
-///*
 #include "H5ExampleClusterCollection.h"
 #include "H5ExampleReferencingTypeCollection.h"
 #include "H5ExampleWithComponentCollection.h"
@@ -25,10 +25,9 @@
 #include "H5ExampleWithNamespaceCollection.h"
 #include "H5ExampleWithARelationCollection.h"
 #include "H5ExampleWithARelationCollection.h"
-//*/
-
 #include "H5ExampleWithStringCollection.h"
 #include "H5ExampleWithArrayCollection.h"
+*/
 
 using namespace H5;
 using namespace std;
@@ -50,7 +49,6 @@ int main()
 
 		auto& info       = store.create<EventInfoCollection>("info");
 		auto& mcps       = store.create<ExampleMCCollection>("mcparticles");
-
 		/*
 		auto& hits       = store.create<ExampleHitCollection>("hits");
 		auto& clusters   = store.create<ExampleClusterCollection>("clusters");
@@ -67,7 +65,7 @@ int main()
 		*/
 
 		writer.registerForWrite<H5EventInfoCollection>("info");
-	    writer.registerForWrite<H5ExampleMCCollection>("mcparticles");
+		writer.registerForWrite<H5ExampleMCCollection>("mcparticles");
 
 		/*
 	    writer.registerForWrite<H5ExampleHitCollection>("hits");
@@ -84,74 +82,101 @@ int main()
 		writer.registerForWrite<H5ExampleWithArrayCollection>("arrays");
 		*/
 
-		unsigned nevents = 2;
+		unsigned nevents = 3;
 
 		for(unsigned i=0; i<nevents; ++i)
 		{
 			std::cout << "processing event " << i << std::endl;
 
-			auto item1 = EventInfo();
-			item1.Number(i);
-			info.push_back(item1);
+			if(i==0)
+			{
+				auto item_1 = EventInfo();
+				item_1.Number(20);
 
-			// ---- add some MC particles ----
-			auto mcp0 = ExampleMC();
-			auto mcp1 = ExampleMC();
-			auto mcp2 = ExampleMC();
-			auto mcp3 = ExampleMC();
-			auto mcp4 = ExampleMC();
-			auto mcp5 = ExampleMC();
-			auto mcp6 = ExampleMC();
-			auto mcp7 = ExampleMC();
-			auto mcp8 = ExampleMC();
-			auto mcp9 = ExampleMC();
+				auto item_2 = EventInfo();
+				item_2.Number(21);
 
-			mcps.push_back( mcp0 ) ;
-			mcps.push_back( mcp1 ) ;
-			mcps.push_back( mcp2 ) ;
-			mcps.push_back( mcp3 ) ;
-			mcps.push_back( mcp4 ) ;
-			mcps.push_back( mcp5 ) ;
-			mcps.push_back( mcp6 ) ;
-			mcps.push_back( mcp7 ) ;
-			mcps.push_back( mcp8 ) ;
-			mcps.push_back( mcp9 ) ;
+				auto item_3 = EventInfo();
+				item_3.Number(22);
 
-			// --- add some daughter relations
-			auto p = ExampleMC();
-			auto d = ExampleMC();
+				auto item_4 = EventInfo();
+				item_4.Number(23);
 
-			p = mcps[0] ;
-			p.adddaughters( mcps[2] ) ;
-			p.adddaughters( mcps[3] ) ;
-			p.adddaughters( mcps[4] ) ;
-			p.adddaughters( mcps[5] ) ;
-			p = mcps[1] ;
-			p.adddaughters( mcps[2] ) ;
-			p.adddaughters( mcps[3] ) ;
-			p.adddaughters( mcps[4] ) ;
-			p.adddaughters( mcps[5] ) ;
-			p = mcps[2] ;
-			p.adddaughters( mcps[6] ) ;
-			p.adddaughters( mcps[7] ) ;
-			p.adddaughters( mcps[8] ) ;
-			p.adddaughters( mcps[9] ) ;
-			p = mcps[3] ;
-			p.adddaughters( mcps[6] ) ;
-			p.adddaughters( mcps[7] ) ;
-			p.adddaughters( mcps[8] ) ;
-			p.adddaughters( mcps[9] ) ;
+				info.push_back(item_1);
+				info.push_back(item_2);
+				info.push_back(item_3);
+				info.push_back(item_4);
 
-			//--- now fix the parent relations
-			for( unsigned j=0,N=mcps.size();j<N;++j){
-			p = mcps[j] ;
-			for(auto it = p.daughters_begin(), end = p.daughters_end() ; it!=end ; ++it ){
-			int dIndex = it->getObjectID().index ;
-			d = mcps[ dIndex ] ;
-			d.addparents( p ) ;
-			}
+				// ---- add some MC particles ----
+				auto mcp0 = ExampleMC();
+				auto mcp1 = ExampleMC();
+				auto mcp2 = ExampleMC();
+				auto mcp3 = ExampleMC();
+
+				mcps.push_back( mcp0 ) ;
+				mcps.push_back( mcp1 ) ;
+				mcps.push_back( mcp2 ) ;
+				mcps.push_back( mcp3 ) ;
+
+				auto p = ExampleMC();
+				auto d = ExampleMC();
+
+				p = mcps[0] ;
+				p.adddaughters( mcps[2] ) ;
+				p.adddaughters( mcps[3] ) ;
+				p = mcps[1] ;
+				p.adddaughters( mcps[2] ) ;
+				p.adddaughters( mcps[3] ) ;
+				//--- now fix the parent relations
+				for( unsigned j=0,N=mcps.size();j<N;++j)
+				{
+					p = mcps[j] ;
+					for(auto it = p.daughters_begin(), end = p.daughters_end() ; it!=end ; ++it )
+					{
+						int dIndex = it->getObjectID().index ;
+						d = mcps[ dIndex ] ;
+						d.addparents( p ) ;
+					}
+				}
 			}
 
+			if(i==1)
+			{
+				auto item_1 = EventInfo();
+				item_1.Number(24);
+
+				auto item_2 = EventInfo();
+				item_2.Number(25);
+
+				info.push_back(item_1);
+				info.push_back(item_2);
+
+
+				// ---- add some MC particles ----
+				auto mcp0 = ExampleMC();
+				auto mcp1 = ExampleMC();
+
+				mcps.push_back( mcp0 ) ;
+				mcps.push_back( mcp1 ) ;
+
+				auto p = ExampleMC();
+				auto d = ExampleMC();
+
+				p = mcps[0] ;
+				p.adddaughters( mcps[1] ) ;
+
+				//--- now fix the parent relations
+				for( unsigned j=0,N=mcps.size();j<N;++j)
+				{
+					p = mcps[j] ;
+					for(auto it = p.daughters_begin(), end = p.daughters_end() ; it!=end ; ++it )
+					{
+						int dIndex = it->getObjectID().index ;
+						d = mcps[ dIndex ] ;
+						d.addparents( p ) ;
+					}
+				}
+			}
 
 
 			writer.writeEvent();
