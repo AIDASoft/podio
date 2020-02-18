ec1968b36d0e68d4a8a77b1eeeb3b59dc5c2af5d
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-02-18 13:24:56 +0100

    [CI] use the Ninja build tool

--- .podio-ci.d/compile_and_test.sh
+++ .podio-ci.d/compile_and_test.sh
@@ -6,7 +6,7 @@ cd /podio
 source init.sh
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 .. && \
-make -j4 && \
-make install && \
-ctest --output-on-failure
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17  -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja .. && \
+    ninja -k0 && \
+    ninja install && \
+    ctest --output-on-failure
