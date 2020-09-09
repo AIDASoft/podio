364a9ce5f013aa980bb0387597bd95acd731ca2d
tmadlener <thomas.madlener@desy.de>
2020-09-09 18:08:59 +0200

    First attempt of splitting core and IO libraries
    Making the core podio library independent of SIO (as it has previously
    also been done with ROOT). In a similar fashion split off the SIO
    dependent parts from the libTestDataModel and try to generate a specific
    blocks library that only contains the SIOBlocks stuff. Currently need to
    inclue at least one of the SIOBlock headers of the library to actually
    force the proper self-registration of the SIOBlocks in the SIOBlock
    factory.

--- tests/write_sio.cpp
+++ tests/write_sio.cpp
@@ -1,6 +1,8 @@
 #include "write_test.h"
 #include "podio/SIOWriter.h"
 
+#include "datamodel/EventInfoSIOBlock.h" // here to get the factory working properly
+
 int main(int argc, char* argv[]){
   write<podio::SIOWriter>("example.sio");
   write<podio::SIOWriter>("example1.sio");
