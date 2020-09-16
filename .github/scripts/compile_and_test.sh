122ed9766281d9772617c5582e61ab4a5e67860a
tmadlener <thomas.madlener@desy.de>
2020-09-16 12:41:59 +0200

    Rename cmake option to enable sio support

--- .github/scripts/compile_and_test.sh
+++ .github/scripts/compile_and_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SIO_HANDLERS=${USE_SIO:-OFF}
+ENABLE_SIO=${USE_SIO:-OFF}
 
 # Depending on whether we need SIO or not setup the environment accordingly
-if [ ${SIO_HANDLERS} = "OFF" ]; then
+if [ ${ENABLE_SIO} = "OFF" ]; then
     source /cvmfs/sft.cern.ch/lcg/views/${VIEW}/setup.sh
 else
     source /cvmfs/clicdp.cern.ch/iLCSoft/${VIEW}/init_ilcsoft.sh
@@ -16,7 +16,7 @@ cd /Package
 source init.sh
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DSIO_HANDLERS=${SIO_HANDLERS} -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja .. && \
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DENABLE_SIO=${ENABLE_SIO} -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja .. && \
 ninja -k0 && \
 ninja install && \
 ctest --output-on-failure
