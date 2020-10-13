3219f558579c257fcfb9fb2f6b7233618a7d7c53
tmadlener <thomas.madlener@desy.de>
2020-10-13 14:40:03 +0200

    Create cmake lists file with generated files

--- cmake/podioMacros.cmake
+++ cmake/podioMacros.cmake
@@ -134,6 +134,7 @@ function(PODIO_GENERATE_DATAMODEL datamodel YAML_FILE RETURN_HEADERS RETURN_SOUR
     # At least build the ROOT selection.xml by default for now
     SET(ARG_IO_BACKEND_HANDLERS "ROOT")
   ENDIF()
+
   # we need to boostrap the data model, so this has to be executed in the cmake run
   execute_process(
     COMMAND ${CMAKE_COMMAND} -E echo "Creating \"${datamodel}\" data model"
@@ -141,19 +142,18 @@ function(PODIO_GENERATE_DATAMODEL datamodel YAML_FILE RETURN_HEADERS RETURN_SOUR
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     )
 
-  file(GLOB headers ${ARG_OUTPUT_FOLDER}/${datamodel}/*.h)
-  file(GLOB sources ${ARG_OUTPUT_FOLDER}/src/*.cc)
-
-  set (${RETURN_HEADERS} ${headers} PARENT_SCOPE)
-  set (${RETURN_SOURCES} ${sources} PARENT_SCOPE)
-
   add_custom_target(create_${datamodel}
     COMMENT "Re-Creating \"${datamodel}\" data model"
     DEPENDS ${YAML_FILE}
-    BYPRODUCTS ${sources} ${headers}
+    BYPRODUCTS ${ARG_OUTPUT_FOLDER}/podio_generated_files.cmake
     COMMAND python ${podio_PYTHON_DIR}/podio_class_generator.py --quiet ${YAML_FILE} ${ARG_OUTPUT_FOLDER} ${datamodel} ${ARG_IO_BACKEND_HANDLERS}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     )
+
+  include(${ARG_OUTPUT_FOLDER}/podio_generated_files.cmake)
+
+  set (${RETURN_HEADERS} ${headers} PARENT_SCOPE)
+  set (${RETURN_SOURCES} ${sources} PARENT_SCOPE)
 
 endfunction()
 
