d101e1a9b1dd946ee49649117f3522aeaacfd843
tmadlener <thomas.madlener@desy.de>
2021-01-26 15:52:07 +0100

    Require -Werror in CI
    Make it harder to accidentally merge new warnings from now on.

--- .github/scripts/compile_and_test.sh
+++ .github/scripts/compile_and_test.sh
@@ -13,7 +13,7 @@ cd /Package
 source init.sh
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DENABLE_SIO=${ENABLE_SIO} -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always " -G Ninja .. && \
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DENABLE_SIO=${ENABLE_SIO} -DCMAKE_CXX_FLAGS=" -fdiagnostics-color=always -Werror" -G Ninja .. && \
 ninja -k0 && \
 ninja install && \
 ctest --output-on-failure
