36a1df73af749be1e6576e918d5a62a8fbab0faf
tmadlener <thomas.madlener@desy.de>
2021-02-25 17:51:52 +0100

    Make use of more matrix capabilities to unify workflows

--- .github/scripts/runPythonChecks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "RUNNING PYLINT PY3K CHECK" && \
-find . -name "*.py" -exec pylint --rcfile=.github/scripts/podio.pylint.py3k.rc --py3k --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" {} + && \
-echo "" && \
-echo "RUNNING FLAKE8 CHECK" && \
-find . -name "*.py" -exec flake8 {} +
-
