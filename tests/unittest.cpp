2d36032cfc0d5f4e42fa58268dadb0a0cf6f1269
Thomas Madlener <thomas.madlener@desy.de>
2021-08-13 17:37:28 +0200

    Switch to Catch2 v3 (#206)
    * Switch to Catch2 v3 for unittests
    
    This makes it possible to build it only once and statically link against
    a central version. For now set the default to pull it from github and
    build it locally nontheless
    
    * Enable using an external Catch2 by default
    
    * Add some clangd tooling to .gitignore
    
    * Update README to include Catch2

--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -5,9 +5,7 @@
 #include <vector>
 #include <type_traits>
 
-// catch
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include "catch2/catch_test_macros.hpp"
 
 // podio specific includes
 #include "podio/EventStore.h"
@@ -146,7 +144,6 @@ TEST_CASE("Invalid_refs") {
 }
 
 TEST_CASE("Looping") {
-  bool success = true;
   auto store = podio::EventStore();
   auto& coll  = store.create<ExampleHitCollection>("name");
   auto hit1 = coll.create(0xbadULL,0.,0.,0.,0.);
@@ -316,7 +313,7 @@ TEST_CASE("Equality") {
 
 TEST_CASE("NonPresentCollection") {
   auto store = podio::EventStore();
-  REQUIRE_THROWS_AS(store.get<ExampleHitCollection>("NonPresentCollection"), std::runtime_error&);
+  REQUIRE_THROWS_AS(store.get<ExampleHitCollection>("NonPresentCollection"), std::runtime_error);
 }
 
 TEST_CASE("const correct indexed access to const collections", "[const-correctness]") {
