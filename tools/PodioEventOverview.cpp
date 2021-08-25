a17b64ca83aefd0388dde7a7e8df14c6dda070cb
jummorenope <jummorenope@unal.edu.co>
2021-08-25 15:26:13 -0500

    Changing scope of the events printed to user defined

--- tools/PodioEventOverview.cpp
+++ tools/PodioEventOverview.cpp
@@ -8,27 +8,36 @@
 
 int main(int argc, char* argv[]){
   
-  if(argc!=2){
+  if(argc<2){
     std::cout<<"ERROR FileName not received"<<std::endl;
-    return 0;
+    return 1;
   }
   std::string FileName{argv[1]};
   
   //Declaring the reader object and openning the file
   auto reader = getReader(FileName);
   reader->openFile(FileName);
-  int evt=reader->getEntries();
-  
+  int eventNumber=reader->getEntries();
+  int readEvent=1;
+  if(argc==3){
+    if(std::stoi(argv[2])>=eventNumber){
+      std::cout<<"Not enough events in file"<<std::endl;
+      return 1;
+    }
+    else{
+      readEvent=std::stoi(argv[2]);
+    }
+  }
   //Printing important info of the file
   std::cout<<"FileName: "<<FileName<<std::endl;
-  std::cout<<"Number of events: "<<evt<<std::endl;
+  std::cout<<"Number of events: "<<eventNumber<<std::endl;
   std::cout<<std::endl;
   
   //Getting Table containing the info about the collections
   const auto collIDTable = reader->getCollectionIDTable();
   const auto collNames = collIDTable->names();
   //Iterating over all events to get size for each event
-  for(int i=0; i<evt; i++){
+  for(int i=0; i<readEvent; i++){
     std::cout<<std::left<<std::setw(30)<<"Name"<<std::left<<std::setw(40)<<"Type"<<"Colection Size"<<std::endl;
     std::cout<<"-------------------------------------------------------------------------------------"<<std::endl;
 
