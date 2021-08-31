c6bd9b0dff04cb6d2a4bbf02df60a67ec9dd7e0e
Frank Gaede <frank.gaede@desy.de>
2021-08-31 19:19:19 +0200

    add long/double types

--- src/SIOBlockUserData.cc
+++ src/SIOBlockUserData.cc
@@ -2,5 +2,8 @@
 #include "podio/UserDataSIOBlock.h"
 
 
-static UserDataSIOBlock<int> _dummyIntCollcetionSIOBlock ;
-static UserDataSIOBlock<float> _dummyFloatCollcetionSIOBlock ;
+// static variable needed for registering in SIOBlock factory
+static UserDataSIOBlock<int>    _dummyIntCollcetionSIOBlock ;
+static UserDataSIOBlock<long>   _dummyLongCollcetionSIOBlock ;
+static UserDataSIOBlock<float>  _dummyFloatCollcetionSIOBlock ;
+static UserDataSIOBlock<double> _dummyDoubleCollcetionSIOBlock ;
