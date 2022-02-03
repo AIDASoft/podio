de2ea8213b9e0ae8f24b9abd2c9d5bf98d9f7d1d
Sanghyun Ko <sang.hyun.ko@cern.ch>
2022-02-03 14:42:42 +0900

    add macro to control versions

--- podioVersion.h
+++ podioVersion.h
@@ -1,4 +1,9 @@
 #ifndef podioVersion_h
 #define podioVersion_h
 #define podio_VERSION @podio_VERSION@
+#define podio_VERSION_MAJOR @podio_VERSION_MAJOR@
+#define podio_VERSION_MINOR @podio_VERSION_MINOR@
+#define podio_VERSION_PATCH @podio_VERSION_PATCH@
+#define podio_VERSION_ENCODE(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#define podio_VERSION_CODE podio_VERSION_ENCODE(podio_VERSION_MAJOR,podio_VERSION_MINOR,podio_VERSION_PATCH)
 #endif
