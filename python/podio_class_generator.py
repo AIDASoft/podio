03f193eed6d7437e4b381d732828f9d10dda822e
soumilbaldota <71919921+soumilbaldota@users.noreply.github.com>
2022-05-30 15:19:43 +0530

    Replace obj_needs_destructor by more general concept (#291)

--- python/podio_class_generator.py
+++ python/podio_class_generator.py
@@ -256,8 +256,8 @@ class ClassGenerator:
     datatype['forward_declarations_obj'] = fwd_declarations
     datatype['includes_obj'] = self._sort_includes(includes)
     datatype['includes_cc_obj'] = self._sort_includes(includes_cc)
-    needs_destructor = datatype['VectorMembers'] or datatype['OneToManyRelations'] or datatype['OneToOneRelations']
-    datatype['obj_needs_destructor'] = needs_destructor
+    trivial_types = datatype['VectorMembers'] or datatype['OneToManyRelations'] or datatype['OneToOneRelations']
+    datatype['is_trivial_type'] = trivial_types
 
   def _preprocess_for_class(self, datatype):
     """Do the preprocessing that is necessary for the classes and Mutable classes"""
