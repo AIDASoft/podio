762885c44d756b42aacaa1d0e3d9c893fcc61601
jmcarcell <jmcarcell@users.noreply.github.com>
2022-11-30 21:39:00 +0100

    Change == to = and tabs to spaces

--- env.sh
+++ env.sh
@@ -8,25 +8,25 @@ if [ -z "$PODIO" ]; then
 fi
 
 unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-	echo $LD_LIBRARY_PATH | grep $PODIO/lib >& /dev/null
-	if [ $? == "1" ]; then
+if [[ "$unamestr" = 'Linux' ]]; then
+  echo $LD_LIBRARY_PATH | grep $PODIO/lib >& /dev/null
+  if [ $? = "1" ]; then
     # RedHat based put the libraries into lib64
     if [ -d $PODIO/lib64 ]; then
       export LD_LIBRARY_PATH=$PODIO/lib64:$LD_LIBRARY_PATH
     else
       export LD_LIBRARY_PATH=$PODIO/lib:$LD_LIBRARY_PATH
     fi
-	fi
-elif [[ "$unamestr" == 'Darwin' ]]; then
+  fi
+elif [[ "$unamestr" = 'Darwin' ]]; then
   # This currenty does not work on OS X as DYLD_LIBRARY_PATH is ignored
   # in recent OS X versions
-	echo $DYLD_LIBRARY_PATH | grep $PODIO/lib >& /dev/null
-	if [ $? == "1" ]; then
-    	export DYLD_LIBRARY_PATH=$PODIO/lib:$DYLD_LIBRARY_PATH
+  echo $DYLD_LIBRARY_PATH | grep $PODIO/lib >& /dev/null
+  if [ $? = "1" ]; then
+      export DYLD_LIBRARY_PATH=$PODIO/lib:$DYLD_LIBRARY_PATH
     fi
 fi
 echo $PYTHONPATH | grep $PODIO/python >& /dev/null
-if [ $? == "1" ]; then
-	export PYTHONPATH=$PODIO/python:$PYTHONPATH
+if [ $? = "1" ]; then
+  export PYTHONPATH=$PODIO/python:$PYTHONPATH
 fi
