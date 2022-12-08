daed17e70f5a2d7d335a6efe10b7aa07460e9350
Thomas Madlener <thomas.madlener@desy.de>
2022-12-08 17:17:25 +0100

    Add registry for datamodel JSON defintions
    - Generate model definitions in JSON format as constexpr string literals
    - Register constexpr string literals in registry

---
+++ include/podio/EDMDefinitionRegistry.h
@@ -0,0 +1,46 @@
+#ifndef PODIO_EDMDEFINITIONREGISTRY_H
+#define PODIO_EDMDEFINITIONREGISTRY_H
+
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+namespace podio {
+class EDMDefinitionRegistry {
+public:
+  static EDMDefinitionRegistry& instance();
+  ~EDMDefinitionRegistry() = default;
+  EDMDefinitionRegistry(const EDMDefinitionRegistry&) = delete;
+  EDMDefinitionRegistry& operator=(const EDMDefinitionRegistry&) = delete;
+  EDMDefinitionRegistry(EDMDefinitionRegistry&&) = delete;
+  EDMDefinitionRegistry& operator=(const EDMDefinitionRegistry&&) = delete;
+
+  /**
+   * Get the definition (in JSON format) of the EDM with the given edm_name. If
+   * no EDM under the given name can be found, an empty model definition is
+   * returned
+   */
+  const std::string_view getDefinition(std::string_view edm_name) const;
+
+  /**
+   * Get the defintion (in JSON format) of the EDM wth the given index. If no
+   * EDM is found under the given index, an empty model definition is returned.
+   */
+  const std::string_view getDefinition(size_t index) const;
+
+  /**
+   * Register a definition and return the index in the registry. If a definition
+   * already exists under the given name, then the index of the existing
+   * definition is returned
+   */
+  size_t registerEDM(std::string name, std::string_view definition);
+
+private:
+  EDMDefinitionRegistry() = default;
+
+  std::vector<std::pair<std::string, std::string_view>> m_definitions{};
+};
+} // namespace podio
+
+#endif // PODIO_EDMDEFINITIONREGISTRY_H
---
+++ src/EDMDefinitionRegistry.cc
@@ -0,0 +1,44 @@
+#include "podio/EDMDefinitionRegistry.h"
+
+#include <algorithm>
+#include <iterator>
+#include <string_view>
+
+namespace podio {
+EDMDefinitionRegistry& EDMDefinitionRegistry::instance() {
+  static EDMDefinitionRegistry registryInstance;
+  return registryInstance;
+}
+
+size_t EDMDefinitionRegistry::registerEDM(std::string name, std::string_view definition) {
+  const auto it = std::find_if(m_definitions.cbegin(), m_definitions.cend(),
+                               [&name](const auto& kvPair) { return kvPair.first == name; });
+
+  if (it == m_definitions.cend()) {
+    int index = m_definitions.size();
+    m_definitions.emplace_back(name, definition);
+    return index;
+  }
+
+  // TODO: Output?
+  return std::distance(m_definitions.cbegin(), it);
+}
+
+const std::string_view EDMDefinitionRegistry::getDefinition(std::string_view edm_name) const {
+  const auto it = std::find_if(m_definitions.cbegin(), m_definitions.cend(),
+                               [&edm_name](const auto& kvPair) { return kvPair.first == edm_name; });
+
+  // TODO: Output when not found
+  return getDefinition(std::distance(m_definitions.cbegin(), it));
+}
+
+const std::string_view EDMDefinitionRegistry::getDefinition(size_t index) const {
+  if (index >= m_definitions.size()) {
+    static constexpr std::string_view emptyDef = "{}"; // valid empty JSON
+    return emptyDef;
+  }
+
+  return m_definitions[index].second;
+}
+
+} // namespace podio
