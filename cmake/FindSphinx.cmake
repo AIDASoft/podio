28af9d67ca2d341b2274e0cb514e59755fd47c0b
Thomas Madlener <thomas.madlener@desy.de>
2023-02-28 11:10:59 +0100

    [wip] Add basic sphinx setup

---
+++ cmake/FindSphinx.cmake
@@ -0,0 +1,8 @@
+find_program(SPHINX_EXE
+  NAMES sphinx-build
+  DOC "Path to sphinx-build executable")
+
+include(FindPackageHandleStandardArgs)
+
+# Handle standard arguments to find_package
+find_package_handle_standard_args(Sphinx "Failed to find sphinx-executable" SPHINX_EXE)
