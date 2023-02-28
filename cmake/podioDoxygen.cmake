2adad88f660b18aeb3be95204c1622c23af95992
Thomas Madlener <thomas.madlener@desy.de>
2023-02-28 09:50:50 +0100

    [doxygen] Make generated pages readable again

--- cmake/podioDoxygen.cmake
+++ cmake/podioDoxygen.cmake
@@ -7,14 +7,14 @@ if(DOXYGEN_FOUND)
   endif()
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/doc/Doxyfile.in
                  ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/doc/doxy-boot.js.in
-                 ${PROJECT_BINARY_DIR}/doxygen/html/doxy-boot.js)
   add_custom_target(doc
                     ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
                     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                     COMMENT "Generating API documentation with Doxygen" VERBATIM)
 
-  install( DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doxygen DESTINATION doxygen OPTIONAL)
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doxygen
+    DESTINATION ${CMAKE_INSTALL_DOCDIR}/doxygen
+    OPTIONAL)
   # revert version to old value
   set(podio_VERSION ${tmp})
   unset(tmp)
