48effcce621f17ccc1b18aea19916bdf420073bb
Thomas Madlener <thomas.madlener@desy.de>
2023-02-28 11:10:59 +0100

    [wip] Add basic sphinx setup

--- cmake/podioDoxygen.cmake
+++ cmake/podioDoxygen.cmake
@@ -1,5 +1,7 @@
 find_package(Doxygen)
-if(DOXYGEN_FOUND)
+find_package(Sphinx)
+
+if(DOXYGEN_FOUND AND SPHINX_FOUND)
   # temporarily override the version for doxy generation (for nightly snapshots)
   set(tmp ${podio_VERSION})
   if(DOXYVERSION)
@@ -12,10 +14,18 @@ if(DOXYGEN_FOUND)
                     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                     COMMENT "Generating API documentation with Doxygen" VERBATIM)
 
+  set(SPHINX_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/sphinx_build)
+  add_custom_target(Sphinx ALL
+                    COMMAND
+                    ${SPHINX_EXE} -M html ${CMAKE_SOURCE_DIR}/doc ${SPHINX_OUTPUT_DIRECTORY}
+                    # DEPENDS doxygen
+                    )
+
   install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doxygen
     DESTINATION ${CMAKE_INSTALL_DOCDIR}/doxygen
     OPTIONAL)
+
   # revert version to old value
   set(podio_VERSION ${tmp})
   unset(tmp)
-endif(DOXYGEN_FOUND)
+endif()
