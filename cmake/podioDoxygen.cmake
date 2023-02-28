2b597a3505c6755170bff2b3c16bacde0c240c0b
Thomas Madlener <thomas.madlener@desy.de>
2023-02-28 15:24:49 +0100

    [wip] Make sphinx pick up doxygen output for c++ API

--- cmake/podioDoxygen.cmake
+++ cmake/podioDoxygen.cmake
@@ -7,22 +7,30 @@ if(DOXYGEN_FOUND AND SPHINX_FOUND)
   if(DOXYVERSION)
     set(podio_VERSION ${DOXYVERSION})
   endif()
+
+  set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/doxygen)
+  file(MAKE_DIRECTORY ${DOXYGEN_OUTPUT_DIRECTORY}) # Doxygen doesn't create this for us
+
+  set(DOXYGEN_FILE ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/doc/Doxyfile.in
-                 ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-  add_custom_target(doc
-                    ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+                 ${DOXYGEN_FILE} @ONLY)
+
+  add_custom_target(doxygen ALL
+                    ${DOXYGEN_EXECUTABLE} ${DOXYGEN_FILE}
                     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                     COMMENT "Generating API documentation with Doxygen" VERBATIM)
 
   set(SPHINX_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/sphinx_build)
   add_custom_target(Sphinx ALL
                     COMMAND
-                    ${SPHINX_EXE} -M html ${CMAKE_SOURCE_DIR}/doc ${SPHINX_OUTPUT_DIRECTORY}
-                    # DEPENDS doxygen
+                    ${SPHINX_EXE} -b html
+                    -Dbreathe_projects.PODIO=${DOXYGEN_OUTPUT_DIRECTORY}/xml
+                    ${CMAKE_SOURCE_DIR}/doc ${SPHINX_OUTPUT_DIRECTORY}
+                    DEPENDS doxygen
                     )
 
-  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doxygen
-    DESTINATION ${CMAKE_INSTALL_DOCDIR}/doxygen
+  install(DIRECTORY ${SPHINX_OUTPUT_DIRECTORY}
+    DESTINATION ${CMAKE_INSTALL_DOCDIR}
     OPTIONAL)
 
   # revert version to old value
