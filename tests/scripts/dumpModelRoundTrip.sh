cd736823cd6c8accf137291e1b877baaf5c9700d
jmcarcell <jmcarcell@users.noreply.github.com>
2023-03-10 18:43:16 +0100

    Add an enable_sio argument

--- tests/scripts/dumpModelRoundTrip.sh
+++ tests/scripts/dumpModelRoundTrip.sh
@@ -3,14 +3,18 @@
 # the original definition. Essentially does not check that the YAML file is the
 # same, but rather that the generated code is the same
 
-set -eu
+set -e
 
-INPUT_FILE=${1}  # the datafile
-EDM_NAME=${2}  # the name of the EDM
+ENABLE_SIO=${1}  # SIO enabled or not
+INPUT_FILE=${2}  # the datafile
+EDM_NAME=${3}  # the name of the EDM
 COMP_BASE_FOLDER=""  # where the source to compare against is
-if [ -$# -gt 2 ]; then
-    COMP_BASE_FOLDER=${3}
+if [ -$# -gt 3 ]; then
+    COMP_BASE_FOLDER=${4}
 fi
+
+echo ${ENABLE_SIO}
+
 
 # Create a few temporary but unique files and directories to store output
 DUMPED_MODEL=${INPUT_FILE}.dumped_${EDM_NAME}.yaml
@@ -20,7 +24,7 @@ mkdir -p ${OUTPUT_FOLDER}
 # Dump the model to a yaml file
 ${PODIO_BASE}/tools/podio-dump --dump-edm ${EDM_NAME} ${INPUT_FILE} > ${DUMPED_MODEL}
 
-# Regenerate the code via the class generator and the freshly dumped modl
+# Regenerate the code via the class generator and the freshly dumped model
 ${PODIO_BASE}/python/podio_class_generator.py \
     --clangformat \
     ${DUMPED_MODEL} \
@@ -31,6 +35,11 @@ ${PODIO_BASE}/python/podio_class_generator.py \
 # Compare to the originally generated code, that has been used to write the data
 # file. Need to diff subfolders explitly here because $PODIO_BASE/tests contains
 # more stuff
-diff -ru ${OUTPUT_FOLDER}/${EDM_NAME} ${PODIO_BASE}/tests/${COMP_BASE_FOLDER}/${EDM_NAME}
+if [ ${ENABLE_SIO} = "false" ]; then
+diff -ru ${OUTPUT_FOLDER}/${EDM_NAME} ${PODIO_BASE}/tests/${COMP_BASE_FOLDER}/${EDM_NAME} --exclude='*SIO*'
+diff -ru ${OUTPUT_FOLDER}/src ${PODIO_BASE}/tests/${COMP_BASE_FOLDER}/src --exclude='*SIO*'
+else
+diff -ru ${OUTPUT_FOLDER}/${EDM_NAME} ${PODIO_BASE}/tests/${COMP_BASE_FOLDER}/${EDM_NAME} 
 diff -ru ${OUTPUT_FOLDER}/src ${PODIO_BASE}/tests/${COMP_BASE_FOLDER}/src
+fi
 diff -u ${OUTPUT_FOLDER}/podio_generated_files.cmake ${PODIO_BASE}/tests/podio_generated_files.cmake
