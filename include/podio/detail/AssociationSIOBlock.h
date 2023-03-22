fab8bc7bf9a4f09dae2eae51e2b0476908007824
Thomas Madlener <thomas.madlener@desy.de>
2023-03-22 10:25:36 +0100

    [wip] Make things compile again after rebase

--- include/podio/detail/AssociationSIOBlock.h
+++ include/podio/detail/AssociationSIOBlock.h
@@ -4,7 +4,6 @@
 #include "podio/detail/AssociationFwd.h"
 
 #include "podio/CollectionBuffers.h"
-
 #include "podio/SIOBlock.h"
 
 #include <sio/api.h>
@@ -19,12 +18,14 @@ template <typename FromT, typename ToT>
 class AssociationSIOBlock : public podio::SIOBlock {
 public:
   AssociationSIOBlock() :
-      SIOBlock(podio::detail::associationSIOName<FromT, ToT>(), sio::version::encode_version(0, 1)) {
+      SIOBlock(podio::detail::associationSIOName<FromT, ToT>(),
+               sio::version::encode_version(AssociationCollection<FromT, ToT>::schemaVersion, 0)) {
     podio::SIOBlockFactory::instance().registerBlockForCollection(podio::detail::associationSIOName<FromT, ToT>(),
                                                                   this);
   }
 
-  AssociationSIOBlock(const std::string& name) : SIOBlock(name, sio::version::encode_version(0, 1)) {
+  AssociationSIOBlock(const std::string& name) :
+      SIOBlock(name, sio::version::encode_version(AssociationCollection<FromT, ToT>::schemaVersion, 0)) {
   }
 
   void read(sio::read_device& device, sio::version_type) override {
@@ -68,20 +69,20 @@ public:
     }
   }
 
-  void createBuffers(const bool subsetCollection = false) override {
-    m_subsetColl = subsetCollection;
+  // void createBuffers(const bool subsetCollection = false) override {
+  //   m_subsetColl = subsetCollection;
 
-    m_buffers.references = new podio::CollRefCollection();
-    m_buffers.vectorMembers = new podio::VectorMembersInfo();
+  //   m_buffers.references = new podio::CollRefCollection();
+  //   m_buffers.vectorMembers = new podio::VectorMembersInfo();
 
-    m_buffers.createCollection = [](podio::CollectionReadBuffers buffers, bool isSubsetColl) {
-      AssociationCollectionData<FromT, ToT> data(buffers, isSubsetColl);
-      return std::make_unique<AssociationCollection<FromT, ToT>>(std::move(data), isSubsetColl);
-    };
+  //   m_buffers.createCollection = [](podio::CollectionReadBuffers buffers, bool isSubsetColl) {
+  //     AssociationCollectionData<FromT, ToT> data(buffers, isSubsetColl);
+  //     return std::make_unique<AssociationCollection<FromT, ToT>>(std::move(data), isSubsetColl);
+  //   };
 
-    // setCollection(new AssociationCollection<FromT, ToT>());
-    // _col->setSubsetCollection(subsetCollection);
-  }
+  //   // setCollection(new AssociationCollection<FromT, ToT>());
+  //   // _col->setSubsetCollection(subsetCollection);
+  // }
 
   SIOBlock* create(const std::string& name) const override {
     return new AssociationSIOBlock(name);
