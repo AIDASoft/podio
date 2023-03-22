065b318d83af882edab6ef2e4572c9202af468b4
Thomas Madlener <thomas.madlener@desy.de>
2023-03-22 18:52:04 +0100

    [wip] Make SIO Frame reading (almost) work again

--- tests/sio_io/write_frame_sio.cpp
+++ tests/sio_io/write_frame_sio.cpp
@@ -1,6 +1,11 @@
+#include "datamodel/ExampleWithARelation.h"
 #include "write_frame.h"
 
 #include "podio/SIOWriter.h"
+
+#include "podio/detail/AssociationSIOBlock.h"
+
+const static auto foo = podio::AssociationSIOBlock<ExampleMC, ex42::ExampleWithARelation>{};
 
 int main(int, char**) {
   write_frames<podio::SIOWriter>("example_frame.sio");
