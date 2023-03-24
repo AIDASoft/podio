06f19307e0a953028a43e9cc104ce89e7dc53a94
Thomas Madlener <thomas.madlener@desy.de>
2023-03-24 15:51:55 +0100

    Make Association I/O work with python

---
+++ tests/DatamodelAssociations.cc
@@ -0,0 +1,6 @@
+#include "podio/AssociationCollection.h"
+
+#include "datamodel/ExampleMCCollection.h"
+#include "datamodel/ExampleWithARelationCollection.h"
+
+PODIO_DECLARE_ASSOCIATION(ExampleMC, ex42::ExampleWithARelation)
