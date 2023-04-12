f4c9219ddde59b1efc73db3fd9c25139222a7b3c
Thomas Madlener <thomas.madlener@desy.de>
2023-04-12 10:04:21 +0200

    Make SIOFrameData work properly with dropped collections (#400)

--- tests/read_frame_sio.cpp
+++ tests/read_frame_sio.cpp
@@ -1,7 +1,9 @@
+#include "read_frame.h"
+#include "read_frame_auxiliary.h"
+
 #include "podio/SIOFrameReader.h"
 
-#include "read_frame.h"
-
 int main() {
-  return read_frames<podio::SIOFrameReader>("example_frame.sio");
+  return read_frames<podio::SIOFrameReader>("example_frame.sio") +
+      test_frame_aux_info<podio::SIOFrameReader>("example_frame.sio");
 }
