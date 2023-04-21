ffc34c1f1fb265e731f95090050e01ddd6a4b59f
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2023-04-21 09:53:42 +0200

    Add an argument to exclude SIO files from some tests (#387)
    * Actually check extension model dumping
    
    * Rework script and CMakeLists to pass in more information in
      environment
    
    * Also check when clang format is used
    ---------
    
    Co-authored-by: jmcarcell <jmcarcell@users.noreply.github.com>
    Co-authored-by: Thomas Madlener <thomas.madlener@desy.de>

--- tests/scripts/dumpModelRoundTrip.sh
+++ tests/scripts/dumpModelRoundTrip.sh
@@ -3,13 +3,16 @@
 # the original definition. Essentially does not check that the YAML file is the
 # same, but rather that the generated code is the same
 
-set -eu
+set -eux
 
 INPUT_FILE=${1}  # the datafile
 EDM_NAME=${2}  # the name of the EDM
-COMP_BASE_FOLDER=""  # where the source to compare against is
-if [ -$# -gt 2 ]; then
-    COMP_BASE_FOLDER=${3}
+COMP_BASE_FOLDER=${3}  # where the source to compare against is
+shift 3
+EXTRA_GEN_ARGS=${@}
+
+if [ ${PODIO_USE_CLANG_FORMAT} = "ON" ] || [ ${PODIO_USE_CLANG_FORMAT} = "AUTO" ]; then
+    EXTRA_GEN_ARGS="${EXTRA_GEN_ARGS} --clangformat"
 fi
 
 # Create a few temporary but unique files and directories to store output
@@ -20,9 +23,9 @@ mkdir -p ${OUTPUT_FOLDER}
 # Dump the model to a yaml file
 ${PODIO_BASE}/tools/podio-dump --dump-edm ${EDM_NAME} ${INPUT_FILE} > ${DUMPED_MODEL}
 
-# Regenerate the code via the class generator and the freshly dumped modl
+# Regenerate the code via the class generator and the freshly dumped model
 ${PODIO_BASE}/python/podio_class_generator.py \
-    --clangformat \
+    ${EXTRA_GEN_ARGS} \
     ${DUMPED_MODEL} \
     ${OUTPUT_FOLDER} \
     ${EDM_NAME} \
@@ -31,6 +34,11 @@ ${PODIO_BASE}/python/podio_class_generator.py \
 # Compare to the originally generated code, that has been used to write the data
 # file. Need to diff subfolders explitly here because $PODIO_BASE/tests contains
 # more stuff
-diff -ru ${OUTPUT_FOLDER}/${EDM_NAME} ${PODIO_BASE}/tests/${COMP_BASE_FOLDER}/${EDM_NAME}
-diff -ru ${OUTPUT_FOLDER}/src ${PODIO_BASE}/tests/${COMP_BASE_FOLDER}/src
-diff -u ${OUTPUT_FOLDER}/podio_generated_files.cmake ${PODIO_BASE}/tests/podio_generated_files.cmake
+DIFF_EXTRA_ARGS=""
+if [ ${ENABLE_SIO} = "OFF" ]; then
+    DIFF_EXTRA_ARGS=--exclude='*SIO*'
+fi
+
+diff -ru ${OUTPUT_FOLDER}/${EDM_NAME} ${COMP_BASE_FOLDER}/${EDM_NAME} ${DIFF_EXTRA_ARGS}
+diff -ru ${OUTPUT_FOLDER}/src ${COMP_BASE_FOLDER}/src ${DIFF_EXTRA_ARGS}
+diff -u ${OUTPUT_FOLDER}/podio_generated_files.cmake ${COMP_BASE_FOLDER}/podio_generated_files.cmake
