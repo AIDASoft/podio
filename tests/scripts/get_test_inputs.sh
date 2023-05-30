fea3033a9eb7f43bbf565615139c8327fa9b210c
tmadlener <thomas.madlener@desy.de>
2023-05-30 17:30:09 +0200

    Rename executable and add more legacy versions for testing

--- tests/scripts/get_test_inputs.sh
+++ tests/scripts/get_test_inputs.sh
@@ -11,5 +11,13 @@ cd ${PODIO_TEST_INPUT_DATA_DIR}
 mkdir v00-13 && cd v00-13
 wget https://key4hep.web.cern.ch:443/testFiles/podio/v00-13/example.root > /dev/null 2>&1
 
+for version in v00-16 v00-16-05; do
+    cd ${PODIO_TEST_INPUT_DATA_DIR}
+    mkdir ${version} && cd ${version}
+    for fileName in example.root example_frame.root; do
+        wget https://key4hep.web.cern.ch:443/testFiles/podio/${version}/${fileName} > /dev/null 2>&1
+    done
+done
+
 # Announce where we store variables to the outside
 echo -n ${PODIO_TEST_INPUT_DATA_DIR}
