f0753109ad83b9dbab5bf19b5fc37b969deb71be
tmadlener <thomas.madlener@desy.de>
2023-05-30 17:15:59 +0200

    Introduce version subdirectory for legacy tests

--- tests/scripts/get_test_inputs.sh
+++ tests/scripts/get_test_inputs.sh
@@ -8,6 +8,7 @@ export PODIO_TEST_INPUT_DATA_DIR
 
 # Get a legacy file with the oldest version that we still support
 cd ${PODIO_TEST_INPUT_DATA_DIR}
+mkdir v00-13 && cd v00-13
 wget https://key4hep.web.cern.ch:443/testFiles/podio/v00-13/example.root > /dev/null 2>&1
 
 # Announce where we store variables to the outside
