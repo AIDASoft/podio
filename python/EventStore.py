9d5a064f5ee383e3332f9b45938a4c2f8c5240c5
tmadlener <thomas.madlener@desy.de>
2023-09-15 16:57:47 +0200

    Remove EventStore python bindings

--- python/EventStore.py
+++
@@ -1,6 +0,0 @@
-"""Legacy import wrapper for EventStore."""
-
-import warnings
-warnings.warn("You are using the legacy EventStore import. Switch to 'from podio import EventStore'", FutureWarning)
-
-from podio import EventStore  # noqa: F401 # pylint: disable=wrong-import-position, unused-import
