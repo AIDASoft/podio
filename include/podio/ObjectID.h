05df4f15dbe0387c4b3715c516c9109b46262941
tmadlener <thomas.madlener@desy.de>
2023-09-18 13:37:03 +0200

    Default initialize ObjectIDs to untracked

--- include/podio/ObjectID.h
+++ include/podio/ObjectID.h
@@ -8,15 +8,15 @@ namespace podio {
 class ObjectID {
 
 public:
-  /// index of object in collection
-  int index;
-  /// ID of the collection
-  uint32_t collectionID;
-
   /// not part of a collection
   static const int untracked = -1;
   /// invalid or non-available object
   static const int invalid = -2;
+
+  /// index of object in collection
+  int index{untracked};
+  /// ID of the collection
+  uint32_t collectionID{static_cast<uint32_t>(untracked)};
 
   /// index and collectionID uniquely defines the object.
   /// this operator is necessary for meaningful comparisons in python
