01e1d05256b924873492e5903402961e9f3362c7
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2023-11-02 09:15:38 +0100

    Use the cmake ExternalData module to manage test data (#508)
    * Use the cmake ExternalData to manage test data
    
    * Remove the macro and script to download files

--- cmake/podioTest.cmake
+++ cmake/podioTest.cmake
@@ -26,27 +26,3 @@ function(CREATE_PODIO_TEST sourcefile additional_libs)
   target_link_libraries(${name} PRIVATE TestDataModel ExtensionDataModel ${additional_libs})
   PODIO_SET_TEST_ENV(${name})
 endfunction()
-
-#--- utility macro to facilitate the downloading of legacy input data
-macro(PODIO_DOWNLOAD_LEGACY_INPUTS legacy_versions)
-  # Avoid fetching these everytime cmake is run by caching the directory the first
-  # time the inputs are fetched or if the expected file does not exist in the
-  # expected directory
-  if (NOT DEFINED CACHE{PODIO_TEST_INPUT_DATA_DIR} OR NOT EXISTS ${PODIO_TEST_INPUT_DATA_DIR}/v00-16-05/example_frame.root)
-    message(STATUS "Getting test input files")
-    execute_process(
-      COMMAND bash ${PROJECT_SOURCE_DIR}/tests/scripts/get_test_inputs.sh ${legacy_versions}
-      OUTPUT_VARIABLE podio_test_input_data_dir
-      RESULT_VARIABLE test_inputs_available
-    )
-    if (NOT "${test_inputs_available}" STREQUAL "0")
-      message(WARNING "Could not get test input files. Will skip some tests that depend on these")
-      # Catch cases where the variable is cached but the file no longer exists
-      unset(PODIO_TEST_INPUT_DATA_DIR CACHE)
-    else()
-      message(STATUS "Test inputs stored in: " ${podio_test_input_data_dir})
-      set(PODIO_TEST_INPUT_DATA_DIR ${podio_test_input_data_dir} CACHE INTERNAL "input dir for test inputs fetched from remote sources")
-      mark_as_advanced(PODIO_TEST_INPUT_DATA_DIR)
-    endif()
-  endif()
-endmacro()
