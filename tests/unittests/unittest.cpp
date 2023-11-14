6abff1dc67f22e1ab9237d9db387b614e6465b89
tmadlener <thomas.madlener@desy.de>
2023-11-14 08:58:56 +0100

    Remove unnecessary header

--- tests/unittests/unittest.cpp
+++ tests/unittests/unittest.cpp
@@ -7,7 +7,6 @@
 #include <type_traits>
 #include <vector>
 
-#include "catch2/catch_template_test_macros.hpp"
 #include "catch2/catch_test_macros.hpp"
 #include "catch2/matchers/catch_matchers_string.hpp"
 #include "catch2/matchers/catch_matchers_vector.hpp"
