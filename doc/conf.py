c3c652b3aa1efa6be8e91c3319dd9ad94aa71c32
tmadlener <thomas.madlener@desy.de>
2023-12-14 10:45:44 +0100

    [wip] First attempt at getting readthedocs setup

--- doc/conf.py
+++ doc/conf.py
@@ -12,18 +12,20 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../python'))
+
+sys.path.insert(0, os.path.abspath("../python"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PODIO'
-copyright = '2023, Key4hep authors'
-author = 'Key4hep authors'
+project = "PODIO"
+copyright = "2023, Key4hep authors"
+author = "Key4hep authors"
+
 
 # The full version, including alpha/beta/rc tags
-version = '@podio_VERSION@'
-release = version
+# version = '@podio_VERSION@'
+# release = version
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,19 +34,19 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-    'breathe',
+    "myst_parser",
+    "breathe",
     # auto generation of python bindings documentation from docstrings
-    'sphinx.ext.napoleon'
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -52,14 +54,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # Make sure that the __init__ docstrings appear as part of the class
 # documentation
-autoclass_content = 'both'
+autoclass_content = "both"
