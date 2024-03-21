7bf8be16cad3f5db73be806d1d212021ca92e7f7
tmadlener <thomas.madlener@desy.de>
2024-03-21 15:09:18 +0100

    Improve and homogenize the docstrings for the ROOT / SIO readers & writers

--- include/podio/ROOTWriter.h
+++ include/podio/ROOTWriter.h
@@ -21,47 +21,80 @@ class Frame;
 class CollectionBase;
 class GenericParameters;
 
+/// The ROOTWriter writes podio files into ROOT files using TTrees.
+///
+/// Each category get's its own TTree. Additionally, there is a podio_metadata
+/// TTree that contains metadata that is necessary for interpreting the files
+/// for reading.
+///
+/// Files written with the ROOTWriter can be read with the ROOTReader.
 class ROOTWriter {
 public:
+  /// Create a ROOTWriter to write to a file.
+  ///
+  /// NOTE: Existing files will be overwritten without warning.
+  ///
+  /// @param filename The path to the file that will be created.
   ROOTWriter(const std::string& filename);
+
+  /// ROOTWriter destructor
+  ///
+  /// This also takes care of writing all the necessary metadata in order to be
+  /// able to read files back again.
   ~ROOTWriter();
 
+  /// The ROOTWriter is not copy-able
   ROOTWriter(const ROOTWriter&) = delete;
+  /// The ROOTWriter is not copy-able
   ROOTWriter& operator=(const ROOTWriter&) = delete;
 
-  /** Store the given frame with the given category. Store all available
-   * collections from the Frame.
-   *
-   * NOTE: The contents of the first Frame that is written in this way
-   * determines the contents that will be written for all subsequent Frames.
-   */
+  /// Store the given frame with the given category.
+  ///
+  /// This stores all available collections from the Frame.
+  /// NOTE: The contents of the first Frame that is written in this way
+  /// determines the contents that will be written for all subsequent Frames.
+  ///
+  /// @param frame    The Frame to store
+  /// @param category The category name under which this Frame should be stored
   void writeFrame(const podio::Frame& frame, const std::string& category);
 
-  /** Store the given Frame with the given category. Store only the
-   * collections that are passed.
-   *
-   * NOTE: The contents of the first Frame that is written in this way
-   * determines the contents that will be written for all subsequent Frames.
-   */
+  /// Store the given Frame with the given category.
+  ///
+  /// This stores only the desired collections and not the complete frame.
+  /// NOTE: The contents of the first Frame that is written in this way
+  /// determines the contents that will be written for all subsequent Frames.
+  ///
+  /// @param frame        The Frame to store
+  /// @param category     The category name under which this Frame should be
+  ///                     stored
+  /// @param collsToWrite The collection names that should be written
   void writeFrame(const podio::Frame& frame, const std::string& category, const std::vector<std::string>& collsToWrite);
 
-  /** Write the current file, including all the necessary metadata to read it again.
-   */
+  /// Write the current file, including all the necessary metadata to read it
+  /// again.
+  ///
+  /// NOTE: The destructor will also call this, so letting a ROOTWriter go out
+  /// of scope is also a viable way to write a readable file
   void finish();
 
-  /** Check whether the collsToWrite are consistent with the state of the passed
-   * category.
-   *
-   * Return two vectors of collection names. The first one contains all the
-   * names that were missing from the collsToWrite but were present in the
-   * category. The second one contains the names that are present in the
-   * collsToWrite only. If both vectors are empty the category and the passed
-   * collsToWrite are consistent.
-   *
-   * NOTE: This will only be a meaningful check if the first Frame of the passed
-   * category has already been written. Also, this check is rather expensive as
-   * it has to effectively do two set differences.
-   */
+  /// Check whether the collsToWrite are consistent with the state of the passed
+  /// category.
+  ///
+  /// NOTE: This will only be a meaningful check if the first Frame of the passed
+  /// category has already been written. Also, this check is rather expensive as
+  /// it has to effectively do two set differences.
+  ///
+  ///
+  /// @param collsToWrite The collection names that should be checked for
+  ///                     consistency
+  /// @param category     The category name for which consistency should be
+  ///                     checked
+  ///
+  /// @returns two vectors of collection names. The first one contains all the
+  /// names that were missing from the collsToWrite but were present in the
+  /// category. The second one contains the names that are present in the
+  /// collsToWrite only. If both vectors are empty the category and the passed
+  /// collsToWrite are consistent.
   std::tuple<std::vector<std::string>, std::vector<std::string>>
   checkConsistency(const std::vector<std::string>& collsToWrite, const std::string& category) const;
 
@@ -72,10 +105,8 @@ private:
   // NOTE: same as in rootUtils.h private header!
   using CollectionInfoT = std::tuple<uint32_t, std::string, bool, unsigned int>;
 
-  /**
-   * Helper struct to group together all necessary state to write / process a
-   * given category. Created during the first writing of a category
-   */
+  /// Helper struct to group together all necessary state to write / process a
+  /// given category. Created during the first writing of a category
   struct CategoryInfo {
     TTree* tree{nullptr};                                   ///< The TTree to which this category is written
     std::vector<root_utils::CollectionBranches> branches{}; ///< The branches for this category
--- include/podio/SIOWriter.h
+++ include/podio/SIOWriter.h
@@ -14,23 +14,61 @@ namespace podio {
 
 class Frame;
 
+/// The SIOWriter writes podio files into SIO files.
+///
+/// Each Frame is stored into an SIO record which are written in the order in
+/// which Frames are written. As part of the metadata a table of content record
+/// is written which is used in the reader to more quickly access the different
+/// records.
+///
+/// Files written with the SIOWriter can be read with the SIOReader
 class SIOWriter {
 public:
+  /// Create a SIOWriter to write to a file.
+  ///
+  /// NOTE: Existing files will be overwritten without warning.
+  ///
+  /// @param filename The path to the file that will be created.
   SIOWriter(const std::string& filename);
+
+  /// SIOWriter destructor
+  ///
+  /// This also takes care of writing all the necessary metadata in order to be
+  /// able to read files back again.
   ~SIOWriter();
 
+  /// The SIOWriter is not copy-able
   SIOWriter(const SIOWriter&) = delete;
+  /// The SIOWriter is not copy-able
   SIOWriter& operator=(const SIOWriter&) = delete;
 
-  /** Write the given Frame with the given category
-   */
+  /// Store the given frame with the given category.
+  ///
+  /// This stores all available collections from the Frame. Frames of the same
+  /// category can be independent of each other, i.e. they can have different
+  /// collection contents.
+  ///
+  /// @param frame    The Frame to store
+  /// @param category The category name under which this Frame should be stored
   void writeFrame(const podio::Frame& frame, const std::string& category);
 
-  /** Write the given Frame with the given category only storing the collections
-   * that are desired via collsToWrite
-   */
+  /// Store the given Frame with the given category.
+  ///
+  /// This stores only the desired collections and not the complete frame.
+  /// Frames of the same category can be independent of each other, i.e. they
+  /// can have different collection contents.
+  ///
+  /// @param frame        The Frame to store
+  /// @param category     The category name under which this Frame should be
+  ///                     stored
+  /// @param collsToWrite The collection names that should be written
   void writeFrame(const podio::Frame& frame, const std::string& category, const std::vector<std::string>& collsToWrite);
 
+  /// Write the current file, including all the necessary metadata to read it
+  /// again.
+  ///
+  /// NOTE: The destructor will also call this, so letting a SIOWriter go out
+  /// of scope is also a viable way to write a readable file
   void finish();
 
 private:
