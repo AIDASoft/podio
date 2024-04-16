82814a8800e246af71077982e270672b71ed6531
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2024-04-16 21:39:08 +0200

    Change "Collcetion" to "Collection" (#587)
    Co-authored-by: jmcarcell <jmcarcell@users.noreply.github.com>

--- src/SIOBlockUserData.cc
+++ src/SIOBlockUserData.cc
@@ -2,17 +2,17 @@
 
 namespace podio {
 
-static SIOBlockUserData<float> _defaultfloatCollcetionSIOBlock;
-static SIOBlockUserData<double> _defaultdoubleCollcetionSIOBlock;
+static SIOBlockUserData<float> _defaultfloatCollectionSIOBlock;
+static SIOBlockUserData<double> _defaultdoubleCollectionSIOBlock;
 
-static SIOBlockUserData<int8_t> _defaultint8_tCollcetionSIOBlock;
-static SIOBlockUserData<int16_t> _defaultint16_tCollcetionSIOBlock;
-static SIOBlockUserData<int32_t> _defaultint32_tCollcetionSIOBlock;
-static SIOBlockUserData<int64_t> _defaultint64_tCollcetionSIOBlock;
+static SIOBlockUserData<int8_t> _defaultint8_tCollectionSIOBlock;
+static SIOBlockUserData<int16_t> _defaultint16_tCollectionSIOBlock;
+static SIOBlockUserData<int32_t> _defaultint32_tCollectionSIOBlock;
+static SIOBlockUserData<int64_t> _defaultint64_tCollectionSIOBlock;
 
-static SIOBlockUserData<uint8_t> _defaultuint8_tCollcetionSIOBlock;
-static SIOBlockUserData<uint16_t> _defaultuint16_tCollcetionSIOBlock;
-static SIOBlockUserData<uint32_t> _defaultuint32_tCollcetionSIOBlock;
-static SIOBlockUserData<uint64_t> _defaultuint64_tCollcetionSIOBlock;
+static SIOBlockUserData<uint8_t> _defaultuint8_tCollectionSIOBlock;
+static SIOBlockUserData<uint16_t> _defaultuint16_tCollectionSIOBlock;
+static SIOBlockUserData<uint32_t> _defaultuint32_tCollectionSIOBlock;
+static SIOBlockUserData<uint64_t> _defaultuint64_tCollectionSIOBlock;
 
 } // namespace podio
