df642d3829e606fa933f6e03984e50eabf3b6936
jmcarcell <jmcarcell@users.noreply.github.com>
2024-05-06 08:09:16 +0200

    Add read_interface_sio to be excluded when using the Address sanitizer

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -74,13 +74,12 @@ if ((NOT "@FORCE_RUN_ALL_TESTS@" STREQUAL "ON") AND (NOT "@USE_SANITIZER@" STREQ
     set(CTEST_CUSTOM_TESTS_IGNORE
       ${CTEST_CUSTOM_TESTS_IGNORE}
 
-      write_sio
       read_sio
       read_and_write_sio
       write_timed_sio
       read_timed_sio
-      write_frame_sio
       read_frame_sio
+      read_interface_sio
       read_frame_legacy_sio
       read_and_write_frame_sio
       )
