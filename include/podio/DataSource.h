cfe4836fdd0fc760928d400533329b2706d66285
Juraj Smiesko <34742917+kjvbrt@users.noreply.github.com>
2024-08-28 15:56:12 +0200

    Add an RDataSource for podio files and collections (#593)
    * Moving RDataSource closer to Podio/EDM4hep
    
    * Adding podio::ROOTDataSource class to the rootmap
    
    * Separating datasource into standalone library
    
    * Adding ON flag to all tests
    
    * The headers should install now
    
    * Other suggested adjustment
    
    * Installing also utilities directory
    
    * Cleanup setup code slightly to avoid unnecessary copies
    
    * Adding missing podioDataSourceDict target
    
    ---------
    
    Co-authored-by: Thomas Madlener <thomas.madlener@desy.de>

---
+++ include/podio/DataSource.h
@@ -0,0 +1,160 @@
+#ifndef PODIO_DATASOURCE_H
+#define PODIO_DATASOURCE_H
+
+// Podio
+#include <podio/CollectionBase.h>
+#include <podio/Frame.h>
+#include <podio/Reader.h>
+
+// ROOT
+#include <ROOT/RDataFrame.hxx>
+#include <ROOT/RDataSource.hxx>
+
+// STL
+#include <memory>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+
+namespace podio {
+class DataSource : public ROOT::RDF::RDataSource {
+public:
+  ///
+  /// @brief Construct the podio::DataSource from the provided file.
+  ///
+  explicit DataSource(const std::string& filePath, int nEvents = -1);
+
+  ///
+  /// @brief Construct the podio::DataSource from the provided file list.
+  ///
+  explicit DataSource(const std::vector<std::string>& filePathList, int nEvents = -1);
+
+  ///
+  /// @brief Inform the podio::DataSource of the desired level of parallelism.
+  ///
+  void SetNSlots(unsigned int nSlots) override;
+
+  ///
+  /// @brief Inform podio::DataSource that an event-loop is about to start.
+  ///
+  void Initialize() override;
+
+  ///
+  /// @brief Retrieve from podio::DataSource a set of ranges of entries that
+  ///        can be processed concurrently.
+  ///
+  std::vector<std::pair<ULong64_t, ULong64_t>> GetEntryRanges() override;
+
+  ///
+  /// @brief Inform podio::DataSource that a certain thread is about to start
+  ///        working on a certain range of entries.
+  ///
+  void InitSlot(unsigned int slot, ULong64_t firstEntry) override;
+
+  ///
+  /// @brief Inform podio::DataSource that a certain thread is about to start
+  ///        working on a certain entry.
+  ///
+  bool SetEntry(unsigned int slot, ULong64_t entry) override;
+
+  ///
+  /// @brief Inform podio::DataSource that a certain thread finished working
+  ///        on a certain range of entries.
+  ///
+  void FinalizeSlot(unsigned int slot) override;
+
+  ///
+  /// @brief Inform podio::DataSource that an event-loop finished.
+  ///
+  void Finalize() override;
+
+  ///
+  /// @brief Returns a reference to the collection of the dataset's column
+  ///        names
+  ///
+  const std::vector<std::string>& GetColumnNames() const override;
+
+  ///
+  /// @brief Checks if the dataset has a certain column.
+  ///
+  bool HasColumn(std::string_view columnName) const override;
+
+  ///
+  /// @brief Type of a column as a string. Required for JITting.
+  ///
+  std::string GetTypeName(std::string_view columnName) const override;
+
+protected:
+  ///
+  /// @brief Type-erased vector of pointers to pointers to column
+  ///        values --- one per slot.
+  ///
+  std::vector<void*> GetColumnReadersImpl(std::string_view name, const std::type_info& typeInfo) override;
+
+  std::string AsString() override {
+    return "Podio data source";
+  };
+
+private:
+  /// Number of slots/threads
+  unsigned int m_nSlots = 1;
+
+  /// Input filename
+  std::vector<std::string> m_filePathList = {};
+
+  /// Total number of events
+  ULong64_t m_nEvents = 0;
+
+  /// Ranges of events available to be processed
+  std::vector<std::pair<ULong64_t, ULong64_t>> m_rangesAvailable = {};
+
+  /// Ranges of events available ever created
+  std::vector<std::pair<ULong64_t, ULong64_t>> m_rangesAll = {};
+
+  /// Column names
+  std::vector<std::string> m_columnNames{};
+
+  /// Column types
+  std::vector<std::string> m_columnTypes = {};
+
+  /// Collections, m_Collections[columnIndex][slotIndex]
+  std::vector<std::vector<const podio::CollectionBase*>> m_Collections = {};
+
+  /// Active collections
+  std::vector<unsigned int> m_activeCollections = {};
+
+  /// Root podio readers
+  std::vector<std::unique_ptr<podio::Reader>> m_podioReaders = {};
+
+  /// Podio frames
+  std::vector<std::unique_ptr<podio::Frame>> m_frames = {};
+
+  ///
+  /// @brief Setup input for the podio::DataSource.
+  ///
+  /// @param[in]  Number of events.
+  /// @return     void.
+  ///
+  void SetupInput(int nEvents);
+};
+
+///
+/// @brief Create RDataFrame from multiple Podio files.
+///
+/// @param[in] filePathList  List of file paths from which the RDataFrame
+///                          will be created.
+/// @return                  RDataFrame created from input file list.
+///
+ROOT::RDataFrame CreateDataFrame(const std::vector<std::string>& filePathList);
+
+///
+/// @brief Create RDataFrame from a Podio file.
+///
+/// @param[in] filePath  File path from which the RDataFrame will be created.
+/// @return              RDataFrame created from input file list.
+///
+ROOT::RDataFrame CreateDataFrame(const std::string& filePath);
+} // namespace podio
+
+#endif /* PODIO_DATASOURCE_H */
---
+++ src/DataSource.cc
@@ -0,0 +1,187 @@
+#include "podio/DataSource.h"
+#include "podio/Reader.h"
+
+// podio
+#include <podio/FrameCategories.h>
+
+// ROOT
+#include <TFile.h>
+
+// STL
+#include <cstddef>
+#include <cstdio>
+#include <memory>
+
+namespace podio {
+DataSource::DataSource(const std::string& filePath, int nEvents) : m_nSlots{1} {
+  m_filePathList.emplace_back(filePath);
+  SetupInput(nEvents);
+}
+
+DataSource::DataSource(const std::vector<std::string>& filePathList, int nEvents) :
+    m_nSlots{1}, m_filePathList{filePathList} {
+  SetupInput(nEvents);
+}
+
+void DataSource::SetupInput(int nEvents) {
+  if (m_filePathList.empty()) {
+    throw std::runtime_error("podio::DataSource: No input files provided!");
+  }
+
+  // Check if the provided file(s) exists and contain required metadata is done
+  // by podio::Reader
+
+  // Create probing frame
+  podio::Frame frame;
+  unsigned int nEventsInFiles = 0;
+  auto podioReader = podio::makeReader(m_filePathList);
+  nEventsInFiles = podioReader.getEntries(podio::Category::Event);
+  frame = podioReader.readFrame(podio::Category::Event, 0);
+
+  // Determine over how many events to run
+  if (nEventsInFiles <= 0) {
+    throw std::runtime_error("podio::DataSource: No events found!");
+  }
+
+  if (nEvents < 0) {
+    m_nEvents = nEventsInFiles;
+  } else if (nEvents == 0) {
+    throw std::runtime_error("podio::DataSource: Requested to run over zero events!");
+  } else {
+    m_nEvents = nEvents;
+  }
+  if (nEventsInFiles < m_nEvents) {
+    m_nEvents = nEventsInFiles;
+  }
+
+  // Get collections stored in the files
+  std::vector<std::string> collNames = frame.getAvailableCollections();
+  for (auto&& collName : collNames) {
+    const podio::CollectionBase* coll = frame.get(collName);
+    if (coll->isValid()) {
+      m_columnNames.emplace_back(std::move(collName));
+      m_columnTypes.emplace_back(coll->getTypeName());
+    }
+  }
+}
+
+void DataSource::SetNSlots(unsigned int nSlots) {
+  m_nSlots = nSlots;
+
+  if (m_nSlots > m_nEvents) {
+    throw std::runtime_error("podio::DataSource: Number of events too small!");
+  }
+
+  int eventsPerSlot = m_nEvents / m_nSlots;
+  for (size_t i = 0; i < (m_nSlots - 1); ++i) {
+    m_rangesAll.emplace_back(eventsPerSlot * i, eventsPerSlot * (i + 1));
+  }
+  m_rangesAll.emplace_back(eventsPerSlot * (m_nSlots - 1), m_nEvents);
+  m_rangesAvailable = m_rangesAll;
+
+  // Initialize set of addresses needed
+  m_Collections.resize(m_columnNames.size(), std::vector<const podio::CollectionBase*>(m_nSlots, nullptr));
+
+  // Initialize podio readers
+  for (size_t i = 0; i < m_nSlots; ++i) {
+    m_podioReaders.emplace_back(std::make_unique<podio::Reader>(podio::makeReader(m_filePathList)));
+  }
+
+  for (size_t i = 0; i < m_nSlots; ++i) {
+    m_frames.emplace_back(std::make_unique<podio::Frame>());
+  }
+}
+
+void DataSource::Initialize() {
+}
+
+std::vector<std::pair<ULong64_t, ULong64_t>> DataSource::GetEntryRanges() {
+  std::vector<std::pair<ULong64_t, ULong64_t>> rangesToBeProcessed;
+  for (auto& range : m_rangesAvailable) {
+    rangesToBeProcessed.emplace_back(range.first, range.second);
+    if (rangesToBeProcessed.size() >= m_nSlots) {
+      break;
+    }
+  }
+
+  if (m_rangesAvailable.size() > m_nSlots) {
+    m_rangesAvailable.erase(m_rangesAvailable.begin(), m_rangesAvailable.begin() + m_nSlots);
+  } else {
+    m_rangesAvailable.erase(m_rangesAvailable.begin(), m_rangesAvailable.end());
+  }
+
+  return rangesToBeProcessed;
+}
+
+void DataSource::InitSlot(unsigned int, ULong64_t) {
+}
+
+bool DataSource::SetEntry(unsigned int slot, ULong64_t entry) {
+  m_frames[slot] = std::make_unique<podio::Frame>(m_podioReaders[slot]->readFrame(podio::Category::Event, entry));
+
+  for (auto& collectionIndex : m_activeCollections) {
+    m_Collections[collectionIndex][slot] = m_frames[slot]->get(m_columnNames.at(collectionIndex));
+  }
+
+  return true;
+}
+
+void DataSource::FinalizeSlot(unsigned int) {
+}
+
+void DataSource::Finalize() {
+}
+
+std::vector<void*> DataSource::GetColumnReadersImpl(std::string_view columnName, const std::type_info&) {
+  auto itr = std::find(m_columnNames.begin(), m_columnNames.end(), columnName);
+  if (itr == m_columnNames.end()) {
+    std::string errMsg = "podio::DataSource: Can't find requested column \"";
+    errMsg += columnName;
+    errMsg += "\"!";
+    throw std::runtime_error(errMsg);
+  }
+  auto columnIndex = std::distance(m_columnNames.begin(), itr);
+  m_activeCollections.emplace_back(columnIndex);
+
+  std::vector<void*> columnReaders(m_nSlots);
+  for (size_t slotIndex = 0; slotIndex < m_nSlots; ++slotIndex) {
+    columnReaders[slotIndex] = (void*)&m_Collections[columnIndex][slotIndex];
+  }
+
+  return columnReaders;
+}
+
+const std::vector<std::string>& DataSource::GetColumnNames() const {
+  return m_columnNames;
+}
+
+bool DataSource::HasColumn(std::string_view columnName) const {
+  return std::find(m_columnNames.begin(), m_columnNames.end(), columnName) != m_columnNames.end();
+}
+
+std::string DataSource::GetTypeName(std::string_view columnName) const {
+  auto itr = std::find(m_columnNames.begin(), m_columnNames.end(), columnName);
+  if (itr == m_columnNames.end()) {
+    std::string errMsg = "podio::DataSource: Type name for \"";
+    errMsg += columnName;
+    errMsg += "\" not found!";
+    throw std::runtime_error(errMsg);
+  }
+
+  auto typeIndex = std::distance(m_columnNames.begin(), itr);
+
+  return m_columnTypes.at(typeIndex);
+}
+
+ROOT::RDataFrame CreateDataFrame(const std::vector<std::string>& filePathList) {
+  ROOT::RDataFrame rdf(std::make_unique<DataSource>(filePathList));
+
+  return rdf;
+}
+
+ROOT::RDataFrame CreateDataFrame(const std::string& filePath) {
+  ROOT::RDataFrame rdf(std::make_unique<DataSource>(filePath));
+
+  return rdf;
+}
+} // namespace podio
