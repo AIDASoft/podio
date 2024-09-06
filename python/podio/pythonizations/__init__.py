c0681ebd9c3d96c6bdf40cd0e31b8d5fdf5f38ba
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-09-06 19:01:08 +0200

    fix pythonizations trying to import local modules as pythonization.submodule

--- python/podio/pythonizations/__init__.py
+++ python/podio/pythonizations/__init__.py
@@ -1,13 +1,16 @@
 """cppyy pythonizations for podio"""
 
 from importlib import import_module
-from pkgutil import walk_packages
+from pkgutil import iter_modules
+from os import path
 from .utils.pythonizer import Pythonizer
 
 
 def load_pythonizations(namespace):
     """Register all available pythonizations for a given namespace"""
-    module_names = [name for _, name, _ in walk_packages(__path__) if not name.startswith("test_")]
+    pythonizations_dir = path.dirname(__file__)
+    # find only direct submodules of the current module
+    module_names = [name for _, name, _ in iter_modules([pythonizations_dir])]
     for module_name in module_names:
         import_module(__name__ + "." + module_name)
     pythonizers = sorted(Pythonizer.__subclasses__(), key=lambda x: x.priority())
