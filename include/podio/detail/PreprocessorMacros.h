5329877e694991a4d74f399ca4761a90f6200e59
Thomas Madlener <thomas.madlener@desy.de>
2024-09-30 15:15:49 +0200

    Fix comment

--- include/podio/detail/PreprocessorMacros.h
+++ include/podio/detail/PreprocessorMacros.h
@@ -2,7 +2,7 @@
 #define PODIO_DETAIL_PREPROCESSORMACROS_H
 
 // Preprocessor helper macros for concatenating tokens at preprocessing times
-// Necessary because we use __COUNTER__ below for unique names of static
+// Necessary because we use __COUNTER__ for unique names of static
 // variables for values returned by registration function calls
 #define PODIO_PP_CONCAT_IMPL(x, y) x##y
 #define PODIO_PP_CONCAT(x, y) PODIO_PP_CONCAT_IMPL(x, y)
