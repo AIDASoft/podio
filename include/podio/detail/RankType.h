fc04c27b33bbf828481dd84a10f568e671e78674
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2024-12-02 09:43:17 +0100

    hide rank value in a proxy

--- include/podio/detail/RankType.h
+++ include/podio/detail/RankType.h
@@ -3,11 +3,23 @@
 #include <cstdint>
 
 namespace podio::detail {
+
+class Rank {
+public:
+  Rank(void* rank) : m_rank(reinterpret_cast<value_type>(rank)) {
+  }
+  friend bool operator<(const Rank& lhs, const Rank& rhs) {
+    return lhs.m_rank < rhs.m_rank;
+  }
+
+private:
 #ifdef UINTPTR_MAX
-using rank_type = uintptr_t;
+  using value_type = uintptr_t;
 #else
-using rank_type = uintmax_t;
+  using value_type = uintmax_t;
 #endif
+  value_type m_rank;
+};
 } // namespace podio::detail
 
 #endif // PODIO_DETAIL_RANKTYPE_H
