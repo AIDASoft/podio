1d0021651a0c04a752796fd3bc256cc139665163
Mateusz Jakub Fila <37295697+m-fila@users.noreply.github.com>
2024-12-03 14:47:04 +0100

    Update include/podio/detail/OrderKey.h
    Co-authored-by: Thomas Madlener <thomas.madlener@desy.de>

--- include/podio/detail/OrderKey.h
+++ include/podio/detail/OrderKey.h
@@ -2,16 +2,16 @@
 #define PODIO_DETAIL_ORDERKEY_H
 #include <functional>
 namespace podio::detail {
-// Internal class allowing datatype objects to be placed in data structures like maps and sets by providing a way to
-// compare them. The comparison is based on addresses of their internal data objects.
-//
-// This class is intended to be used as the return value of internal `podio::detail::getOrderKey` free functions. These
-// functions are friends of each datatype, allowing them to access the internal data objects and define less-than
-// comparison operators for both datatypes and interface types.
-//
-// The friend free function design is used in order to reduce the coupling between interfaces and datatypes. Interfaces
-// do not need to be friends of datatypes to define the less-than comparison operator, which allows using datatypes from
-// different datamodels in an interface type.
+/// Internal class allowing datatype objects to be placed in data structures like maps and sets by providing a way to
+/// compare them. The comparison is based on addresses of their internal data objects.
+///
+/// This class is intended to be used as the return value of internal `podio::detail::getOrderKey` free functions. These
+/// functions are friends of each datatype, allowing them to access the internal data objects and define less-than
+/// comparison operators for both datatypes and interface types.
+///
+/// The friend free function design is used in order to reduce the coupling between interfaces and datatypes. Interfaces
+/// do not need to be friends of datatypes to define the less-than comparison operator, which allows using datatypes from
+/// different datamodels in an interface type.
 class OrderKey {
 public:
   OrderKey(void* orderKey) noexcept : m_orderKey(orderKey) {
