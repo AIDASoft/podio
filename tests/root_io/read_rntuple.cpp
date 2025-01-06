d90d596ae8ad8e987b40fc9681c1eeaefdf19594
Thomas Madlener <thomas.madlener@desy.de>
2025-01-06 19:41:37 +0100

    Also check auxiliary information for RNTuple

--- tests/root_io/read_rntuple.cpp
+++ tests/root_io/read_rntuple.cpp
@@ -1,6 +1,10 @@
-#include "podio/RNTupleReader.h"
 #include "read_frame.h"
+#include "read_frame_auxiliary.h"
+
+#include "podio/RNTupleReader.h"
 
 int main() {
-  return read_frames<podio::RNTupleReader>("example_rntuple.root");
+  const std::string inputFile = "example_rntuple.root";
+
+  return read_frames<podio::RNTupleReader>(inputFile) + test_frame_aux_info<podio::RNTupleReader>(inputFile);
 }
