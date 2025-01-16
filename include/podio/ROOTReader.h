a4aad20e53ecda43b27a006d45a8259bb880e7fc
Thomas Madlener <thomas.madlener@desy.de>
2025-01-16 15:15:27 +0100

    Replace a pair with a struct
    Facilitates the usage of projections with range algorithms

--- include/podio/ROOTReader.h
+++ include/podio/ROOTReader.h
@@ -28,6 +28,10 @@ namespace detail {
   // vector
   using CollectionInfo = std::tuple<std::string, bool, SchemaVersionT, size_t>;
 
+  struct NamedCollInfo {
+    std::string name{};
+    CollectionInfo info{};
+  };
 } // namespace detail
 
 class CollectionBase;
@@ -152,12 +156,12 @@ private:
     /// constructor from chain for more convenient map insertion
     CategoryInfo(std::unique_ptr<TChain>&& c) : chain(std::move(c)) {
     }
-    std::unique_ptr<TChain> chain{nullptr};                                      ///< The TChain with the data
-    unsigned entry{0};                                                           ///< The next entry to read
-    std::vector<std::pair<std::string, detail::CollectionInfo>> storedClasses{}; ///< The stored collections in this
-                                                                                 ///< category
-    std::vector<root_utils::CollectionBranches> branches{};                      ///< The branches for this category
-    std::shared_ptr<CollectionIDTable> table{nullptr}; ///< The collection ID table for this category
+    std::unique_ptr<TChain> chain{nullptr};                 ///< The TChain with the data
+    unsigned entry{0};                                      ///< The next entry to read
+    std::vector<detail::NamedCollInfo> storedClasses{};     ///< The stored collections in this
+                                                            ///< category
+    std::vector<root_utils::CollectionBranches> branches{}; ///< The branches for this category
+    std::shared_ptr<CollectionIDTable> table{nullptr};      ///< The collection ID table for this category
   };
 
   /// Initialize the passed CategoryInfo by setting up the necessary branches,
