a6d9f6fc3a69f20ddc30c4f8d6f17172f49c6a6f
Thomas Madlener <thomas.madlener@desy.de>
2025-02-03 21:07:34 +0100

    Add possibility to limit collections to RDataSource

--- tests/root_io/read_with_rdatasource_root.cpp
+++ tests/root_io/read_with_rdatasource_root.cpp
@@ -1,6 +1,8 @@
 #include "datamodel/ExampleClusterCollection.h"
 #include "podio/DataSource.h"
+#include "podio/Reader.h"
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 
@@ -28,8 +30,40 @@ int main(int argc, const char* argv[]) {
   dframe.Describe().Print();
   std::cout << std::endl;
 
+  const auto expectedCollNames = [&inputFile]() {
+    auto reader = podio::makeReader(inputFile);
+    auto cols = reader.readNextEvent().getAvailableCollections();
+    std::ranges::sort(cols);
+    return cols;
+  }();
+  const auto allColNames = [&dframe]() {
+    auto cols = dframe.GetColumnNames();
+    std::ranges::sort(cols);
+    return cols;
+  }();
+
+  if (!std::ranges::equal(expectedCollNames, allColNames)) {
+    std::cerr << "Column names are note as expected\n  expected: [";
+    for (const auto& name : expectedCollNames) {
+      std::cerr << name << " ";
+    }
+    std::cerr << "]\n    actual: [";
+    for (const auto& name : allColNames) {
+      std::cerr << name << " ";
+    }
+    std::cerr << "]" << std::endl;
+
+    return EXIT_FAILURE;
+  }
+
   auto cluterEnergy = dframe.Define("cluster_energy", getEnergy, {"clusters"}).Histo1D("cluster_energy");
   cluterEnergy->Print();
+
+  dframe = podio::CreateDataFrame(inputFile, {"hits"});
+  if (dframe.GetColumnNames()[0] != "hits") {
+    std::cerr << "Limiting to only one collection didn't work as expected" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
