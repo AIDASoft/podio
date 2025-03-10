85a9bb5c5497fef47a28792ea9a4c1612216b527
Mateusz Jakub Fila <mateusz.jakub.fila@cern.ch>
2025-03-10 13:10:02 +0100

    allocate space for null-termination after concatenated string

--- include/podio/utilities/StaticConcatenate.h
+++ include/podio/utilities/StaticConcatenate.h
@@ -10,14 +10,15 @@ namespace podio::utils {
 template <const std::string_view&... strs>
 struct static_concatenate {
   static constexpr auto init_arr() {
-    constexpr auto total_size = (strs.size() + ... + 0);
+    constexpr auto total_size = (strs.size() + ... + 1); // reserve space for '\0'
     auto arr = std::array<char, total_size>();
     auto it = arr.begin();
     ((it = std::ranges::copy(strs, it).out), ...);
+    arr.back() = '\0';
     return arr;
   }
   constexpr static auto array = init_arr();
-  constexpr static auto value = std::string_view(array.data(), array.size());
+  constexpr static auto value = std::string_view(array.data(), array.size() - 1); // skip '\0'
 };
 
 /// Variable template for concatenating a set of string_views into a single string_view at compile time
