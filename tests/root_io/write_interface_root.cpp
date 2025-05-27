3369a05e13466cec91bd4e5dc5178c1a60f29dfd
jmcarcell <jmcarcell@users.noreply.github.com>
2025-05-27 16:31:01 +0200

    Change the default of the writer to RNTuple when PODIO_WRITE_RNTUPLE
    is set. Add a test using this environment variable and add some documentation
    for the readers and writers in C++ and Python.

--- tests/root_io/write_interface_root.cpp
+++ tests/root_io/write_interface_root.cpp
@@ -1,8 +1,19 @@
 #include "write_interface.h"
 
-int main(int, char**) {
+#include <string>
 
-  auto writer = podio::makeWriter("example_frame_interface.root");
+int main(int argc, char* argv[]) {
+
+  // Check if the user provided a filename as an argument
+  std::string outputFile = "example_frame_interface.root";
+  if (argc == 2) {
+    outputFile = argv[1];
+  } else if (argc > 2) {
+    std::cerr << "Usage: " << argv[0] << " [output_file_name]" << std::endl;
+    return 1;
+  }
+
+  auto writer = podio::makeWriter(outputFile);
   write_frames(writer);
 
   return 0;
