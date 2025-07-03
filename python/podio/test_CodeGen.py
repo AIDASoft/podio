1e589ffb9d9304c94d42720a339e5024552a5a67
jmcarcell <jmcarcell@users.noreply.github.com>
2025-07-03 21:52:44 +0200

    Raise IndexError from the Python API

--- python/podio/test_CodeGen.py
+++ python/podio/test_CodeGen.py
@@ -48,7 +48,7 @@ class CollectionSubscriptTest(unittest.TestCase):
         collection = nsp.EnergyInNamespaceCollection()
         _ = collection.create()
         self.assertEqual(len(collection), 1)
-        with self.assertRaises(cppyy.gbl.std.out_of_range):
+        with self.assertRaises(IndexError):
             _ = collection[20]
         _ = collection[0]
 
