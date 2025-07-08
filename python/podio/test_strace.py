c55c63cca0cdd343b56861c0194c664a27baf4de
jmcarcell <jmcarcell@users.noreply.github.com>
2025-07-08 14:37:39 +0200

    Add Links and a comment

--- python/podio/test_strace.py
+++ python/podio/test_strace.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+"""Create objects and collections to test strace output"""
+
+
+from ROOT import ExampleHitCollection, ExampleHit, TestLink, TestLinkCollection, nsp
 import podio
-from ROOT import ExampleHitCollection, ExampleHit, nsp
 
 coll = ExampleHitCollection()
 coll.create()
@@ -13,3 +17,7 @@ ncoll.create()
 nhit = nsp.EnergyInNamespace()
 nhit.clone()
 
+link_coll = TestLinkCollection()
+link_coll.create()
+link_coll[0].clone()
+link = TestLink()
