d47f943fbd6e5393bb7fc456b13be3be5e9a84b6
Thomas Madlener <thomas.madlener@desy.de>
2025-08-19 17:16:43 +0200

    Make it possible to define tests for RNTuple as well

--- tests/schema_evolution/code_gen/components_new_member/check.cpp
+++ tests/schema_evolution/code_gen/components_new_member/check.cpp
@@ -2,27 +2,21 @@
 
 #include "check_base.h"
 
-#include "podio/ROOTReader.h"
-#include "podio/ROOTWriter.h"
-
 int main() {
-  WRITE_WITH(podio::ROOTWriter, "components_new_member.root", TestTypeCollection, {
+  WRITE_AS(TestTypeCollection, {
     elem.s().x = 42;
     elem.s().y = 43;
     elem.s().z = 44;
     elem.s().p = {1, 2, 3, 4};
   });
-  READ_WITH(podio::ROOTReader, "components_new_member.root", TestTypeCollection,
-            {
-              ASSERT_EQUAL(elem.s().t, 0, "New component member variable is not zero initialized");
-              ASSERT_EQUAL(elem.s().x, 42, "Member variable unrelated to schema evolution has changed");
-              ASSERT_EQUAL(elem.s().y, 43, "Member variable unrelated to schema evolution has changed");
-              ASSERT_EQUAL(elem.s().z, 44, "Member variable unrelated to schema evolution has changed");
-              ASSERT_EQUAL(elem.s().p[0], 1, "Member variable unrelated to schema evolution has changed");
-              ASSERT_EQUAL(elem.s().p[1], 2, "Member variable unrelated to schema evolution has changed");
-              ASSERT_EQUAL(elem.s().p[2], 3, "Member variable unrelated to schema evolution has changed");
-              ASSERT_EQUAL(elem.s().p[3], 4, "Member variable unrelated to schema evolution has changed");
-            }
-
-  );
+  READ_AS(TestTypeCollection, {
+    ASSERT_EQUAL(elem.s().t, 0, "New component member variable is not zero initialized");
+    ASSERT_EQUAL(elem.s().x, 42, "Member variable unrelated to schema evolution has changed");
+    ASSERT_EQUAL(elem.s().y, 43, "Member variable unrelated to schema evolution has changed");
+    ASSERT_EQUAL(elem.s().z, 44, "Member variable unrelated to schema evolution has changed");
+    ASSERT_EQUAL(elem.s().p[0], 1, "Member variable unrelated to schema evolution has changed");
+    ASSERT_EQUAL(elem.s().p[1], 2, "Member variable unrelated to schema evolution has changed");
+    ASSERT_EQUAL(elem.s().p[2], 3, "Member variable unrelated to schema evolution has changed");
+    ASSERT_EQUAL(elem.s().p[3], 4, "Member variable unrelated to schema evolution has changed");
+  });
 }
--- tests/schema_evolution/code_gen/datatypes_new_member/check.cpp
+++ tests/schema_evolution/code_gen/datatypes_new_member/check.cpp
@@ -2,25 +2,19 @@
 
 #include "check_base.h"
 
-#include "podio/ROOTReader.h"
-#include "podio/ROOTWriter.h"
-
 int main() {
-  WRITE_WITH(podio::ROOTWriter, "datatypes_new_member.root", ExampleHitCollection, {
+  WRITE_AS(ExampleHitCollection, {
     elem.x(1.23);
     elem.y(1.23);
     elem.z(1.23);
     elem.cellID(0xcaffee);
   });
-  READ_WITH(podio::ROOTReader, "datatypes_new_member.root", ExampleHitCollection,
-            {
-              ASSERT_EQUAL(elem.t(), 0, "New datatype member variable not 0 initialized");
-              ASSERT_EQUAL(elem.x(), 1.23, "Member variables unrelated to schema evolution have changed");
-              ASSERT_EQUAL(elem.y(), 1.23, "Member variables unrelated to schema evolution have changed");
-              ASSERT_EQUAL(elem.z(), 1.23, "Member variables unrelated to schema evolution have changed");
-              ASSERT_EQUAL(elem.energy(), 0, "Member variables unrelated to schema evolution have changed");
-              ASSERT_EQUAL(elem.cellID(), 0xcaffee, "Member variables unrelated to schema evolution have changed");
-            }
-
-  );
+  READ_AS(ExampleHitCollection, {
+    ASSERT_EQUAL(elem.t(), 0, "New datatype member variable not 0 initialized");
+    ASSERT_EQUAL(elem.x(), 1.23, "Member variables unrelated to schema evolution have changed");
+    ASSERT_EQUAL(elem.y(), 1.23, "Member variables unrelated to schema evolution have changed");
+    ASSERT_EQUAL(elem.z(), 1.23, "Member variables unrelated to schema evolution have changed");
+    ASSERT_EQUAL(elem.energy(), 0, "Member variables unrelated to schema evolution have changed");
+    ASSERT_EQUAL(elem.cellID(), 0xcaffee, "Member variables unrelated to schema evolution have changed");
+  });
 }
