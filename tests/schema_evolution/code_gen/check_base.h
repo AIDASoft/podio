08951a3a49c64869d815c2d84f08f22c7e6b151f
Thomas Madlener <thomas.madlener@desy.de>
2025-08-19 15:55:33 +0200

    Move c++ helpers into common header and add documentation

---
+++ tests/schema_evolution/code_gen/check_base.h
@@ -0,0 +1,51 @@
+#ifndef PODIO_TESTS_SCHEMAEVOLUTION_CODEGEN_CHECKBASE_H // NOLINT(llvm-header-guard): folder structure not suitable
+#define PODIO_TESTS_SCHEMAEVOLUTION_CODEGEN_CHECKBASE_H // NOLINT(llvm-header-guard): folder structure not suitable
+
+#include <podio/Frame.h>
+
+#include <iostream>
+
+#define ASSERT_EQUAL(actual, expected, msg)                                                                            \
+  if ((expected) != (actual)) {                                                                                        \
+    std::cerr << "Assertion failed " << #expected << " == " << #actual << " | " << msg << " (expected: " << expected   \
+              << ", actual: " << actual << ") at " << __FILE__ << ":" << __LINE__;                                     \
+    return 1;                                                                                                          \
+  }
+
+#ifdef PODIO_SCHEMA_EVOLUTION_TEST_WRITE
+  #define WRITE_WITH(WRITER_TYPE, FILENAME, COLL_TYPE, ...)                                                            \
+    {{std::cout << "Writing file " << FILENAME << std::endl;                                                           \
+    auto writer = WRITER_TYPE(FILENAME);                                                                               \
+    auto event = podio::Frame();                                                                                       \
+    {                                                                                                                  \
+      auto coll = COLL_TYPE();                                                                                         \
+      auto elem = coll.create();                                                                                       \
+      __VA_ARGS__                                                                                                      \
+      event.put(std::move(coll), "evolution_collection");                                                              \
+    }                                                                                                                  \
+    writer.writeFrame(event, podio::Category::Event);                                                                  \
+    }                                                                                                                  \
+    }
+#else
+  #define WRITE_WITH(...)
+#endif
+
+#ifdef PODIO_SCHEMA_EVOLUTION_TEST_READ
+  #define READ_WITH(READER_TYPE, FILENAME, COLL_TYPE, ...)                                                             \
+    {{auto reader = READER_TYPE();                                                                                     \
+    std::cout << "Reading file " << FILENAME << std::endl;                                                             \
+    reader.openFile(FILENAME);                                                                                         \
+    auto event = podio::Frame(reader.readEntry(podio::Category::Event, 0));                                            \
+    const auto& coll = event.get<COLL_TYPE>("evolution_collection");                                                   \
+    {                                                                                                                  \
+      const auto elem = coll[0];                                                                                       \
+      __VA_ARGS__                                                                                                      \
+    }                                                                                                                  \
+    return 0;                                                                                                          \
+    }                                                                                                                  \
+    }
+#else
+  #define READ_WITH(...)
+#endif
+
+#endif // PODIO_TESTS_SCHEMAEVOLUTION_CODEGEN_CHECKBASE_H
