c0a12f9b1005a89c94976194153f291ed118e7ed
Thomas Madlener <thomas.madlener@desy.de>
2025-08-22 11:39:00 +0200

    Make macros a bit more generically usable and document

--- tests/schema_evolution/code_gen/check_base.h
+++ tests/schema_evolution/code_gen/check_base.h
@@ -33,40 +33,71 @@ using ReaderT = podio::ROOTReader;
   }
 
 #ifdef PODIO_SCHEMA_EVOLUTION_TEST_WRITE
-  #define WRITE_WITH(WRITER_TYPE, FILENAME, COLL_TYPE, ...)                                                            \
+  /// Macro to write a frame using the specified writer type and filename
+  /// setting up the corresponding writer and a Frame named event. Wraps the
+  /// passed code into a dedicated scope.
+  ///
+  /// @param WRITER_TYPE The type of writer to use (e.g., WriterT)
+  /// @param FILENAME The name of the file to write to
+  #define WRITE_WITH(WRITER_TYPE, FILENAME, ...)                                                                       \
     {{std::cout << "Writing file " << FILENAME << std::endl;                                                           \
     auto writer = WRITER_TYPE(FILENAME);                                                                               \
     auto event = podio::Frame();                                                                                       \
-    {                                                                                                                  \
-      auto coll = COLL_TYPE();                                                                                         \
-      auto elem = coll.create();                                                                                       \
-      __VA_ARGS__                                                                                                      \
-      event.put(std::move(coll), "evolution_collection");                                                              \
-    }                                                                                                                  \
+    {__VA_ARGS__};                                                                                                     \
     writer.writeFrame(event, podio::Category::Event);                                                                  \
     }                                                                                                                  \
     }
-  #define WRITE_AS(COLL_TYPE, ...) {WRITE_WITH(WriterT, (TEST_CASE FILE_SUFFIX), COLL_TYPE, __VA_ARGS__)}
+
+  /// Macro to write a collection of the specified type to a test file using a
+  /// pre-defined WriterT and constructing the filename to write to from the
+  /// TEST_CASE and FILE_SUFFIX pre-processor constants. In addition to setting
+  /// up a writer and an event Frame it also creates the collection of type
+  /// COLL_TYPE and a first element using coll and elem as variable names
+  /// respectively. Wraps the passed code into a dedicated scope
+  ///
+  /// @param COLL_TYPE The type of collection to create and write
+  #define WRITE_AS(COLL_TYPE, ...)                                                                                     \
+    {WRITE_WITH(WriterT, (TEST_CASE FILE_SUFFIX), {                                                                    \
+      auto coll = COLL_TYPE();                                                                                         \
+      auto elem = coll.create();                                                                                       \
+      {__VA_ARGS__};                                                                                                   \
+      event.put(std::move(coll), "evolution_collection");                                                              \
+    })}
 #else
   #define WRITE_WITH(...)
   #define WRITE_AS(...)
 #endif
 
 #ifdef PODIO_SCHEMA_EVOLUTION_TEST_READ
-  #define READ_WITH(READER_TYPE, FILENAME, COLL_TYPE, ...)                                                             \
+  /// Macro to read a frame using the specified reader type and filename. Wraps
+  /// the specified code into a dedicated scope after setting up the reader and
+  /// reading the event Frame.
+  ///
+  /// @param READER_TYPE The type of reader to use
+  /// @param FILENAME The name of the file to read from
+  #define READ_WITH(READER_TYPE, FILENAME, ...)                                                                        \
     {{auto reader = READER_TYPE();                                                                                     \
     std::cout << "Reading file " << FILENAME << std::endl;                                                             \
     reader.openFile(FILENAME);                                                                                         \
     auto event = podio::Frame(reader.readEntry(podio::Category::Event, 0));                                            \
-    const auto& coll = event.get<COLL_TYPE>("evolution_collection");                                                   \
-    {                                                                                                                  \
-      const auto elem = coll[0];                                                                                       \
-      __VA_ARGS__                                                                                                      \
-    }                                                                                                                  \
+    {__VA_ARGS__};                                                                                                     \
     return 0;                                                                                                          \
     }                                                                                                                  \
     }
-  #define READ_AS(COLL_TYPE, ...) {READ_WITH(ReaderT, (TEST_CASE FILE_SUFFIX), COLL_TYPE, __VA_ARGS__)}
+
+  /// Macro to read a collection of the specified type from a test file using
+  /// the READ_WITH macro using the a pre-defined ReaderT and constructing the
+  /// filename to read from from the TEST_CASE and FILE_SUFFIX pre-processor
+  /// constants. Reads the collection of type COLL_TYPE and retrieves the first
+  /// element from it before passing on to the specified code.
+  ///
+  /// @param COLL_TYPE The type of collection to read
+  #define READ_AS(COLL_TYPE, ...)                                                                                      \
+    {READ_WITH(ReaderT, (TEST_CASE FILE_SUFFIX), {                                                                     \
+      const auto& coll = event.get<COLL_TYPE>("evolution_collection");                                                 \
+      const auto elem = coll[0];                                                                                       \
+      {__VA_ARGS__};                                                                                                   \
+    })}
 #else
   #define READ_WITH(...)
   #define READ_AS(...)
