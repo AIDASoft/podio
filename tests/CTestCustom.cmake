01cf98b210f2eb19f38ce21e980c1b0d7455eae1
Thomas Madlener <thomas.madlener@desy.de>
2025-09-01 16:49:21 +0200

    Make ADD_SCHEMA_EVOLUTION_TEST accept multiple old versions

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -9,27 +9,39 @@
 
 # We define some lists here to avoid having to repeat them below
 set(failing_with_address_sanitizer
-  schema_evol:code_gen:datatypes_new_member:read_garbage
-  schema_evol:code_gen:datatypes_rename_relation:write
-  schema_evol:code_gen:datatypes_rename_relation:read
-  schema_evol:code_gen:datatypes_rename_relation:write_rntuple
-  schema_evol:code_gen:datatypes_rename_relation:read_rntuple
+  # Technically most of the write tests here succeed, but there is no point in
+  # only running them, since they are practically only the setup step for the
+  # actual read tests
+  schema_evol:code_gen:components_new_member:write_old
+  schema_evol:code_gen:components_new_member:read
+  schema_evol:code_gen:components_rename_member:write_old
+  schema_evol:code_gen:components_rename_member:read
+  schema_evol:code_gen:datatypes_new_member:write_old
+  schema_evol:code_gen:datatypes_new_member:read
+  schema_evol:code_gen:datatypes_rename_member:write_old
+  schema_evol:code_gen:datatypes_rename_member:read
+  schema_evol:code_gen:implicit_floating_point_change:write_old
+  schema_evol:code_gen:implicit_floating_point_change:read
+  schema_evol:code_gen:no_change:write_old
+  schema_evol:code_gen:no_change:read
+  schema_evol:code_gen:array_component_new_member:write_old
+  schema_evol:code_gen:array_component_new_member:read
 )
 
 set(failing_with_thread_sanitizer
   # Technically most of the write tests here succeed, but there is no point in
   # only running them, since they are practically only the setup step for the
   # actual read tests
-  schema_evol:code_gen:components_new_member:write_rntuple
+  schema_evol:code_gen:components_new_member:write_old_rntuple
   schema_evol:code_gen:components_new_member:read_rntuple
-  schema_evol:code_gen:datatypes_new_member:write_rntuple
+  schema_evol:code_gen:datatypes_new_member:write_old_rntuple
   schema_evol:code_gen:datatypes_new_member:read_rntuple
   schema_evol:code_gen:datatypes_new_member:read_garbage
   schema_evol:code_gen:implicit_floating_point_change:write_rntuple
   schema_evol:code_gen:implicit_floating_point_change:read_rntuple
-  schema_evol:code_gen:no_change:write_rntuple
+  schema_evol:code_gen:no_change:write_old_rntuple
   schema_evol:code_gen:no_change:read_rntuple
-  schema_evol:code_gen:array_component_new_member:write_rntuple
+  schema_evol:code_gen:array_component_new_member:write_old_rntuple
   schema_evol:code_gen:array_component_new_member:read_rntuple
   # The following two work with clang19, but not gcc15
   schema_evol:code_gen:datatypes_remove_type:write_rntuple
