83bba51dee0ea77b5754be1610b9c6e75e098dc4
Thomas Madlener <thomas.madlener@desy.de>
2025-10-20 12:50:07 +0200

    Add some useful static constructors for good defaults

--- include/podio/ReadOptions.h
+++ include/podio/ReadOptions.h
@@ -8,6 +8,14 @@ namespace podio {
 struct ReadOptions {
   std::vector<std::string> collsToRead{};
   bool skipUnredable{false};
+
+  inline static ReadOptions SkipUnreadable() {
+    return {{}, true};
+  }
+
+  inline static ReadOptions Only(std::vector<std::string> collections) {
+    return {std::move(collections), false};
+  }
 };
 } // namespace podio
 
