531a6bd11097cade6c4850deb4f428c9d229e9cc
Thomas Madlener <thomas.madlener@desy.de>
2025-10-20 19:23:25 +0200

    Make it possible to check non supported evolution behavior

---
+++ tests/schema_evolution/code_gen/datatypes_rename_relation/check.cpp
@@ -0,0 +1,34 @@
+#include "datamodel/ExampleClusterCollection.h"
+#include "datamodel/ExampleHitCollection.h"
+
+#include "check_base.h"
+
+int main() {
+  WRITE_WITH(WriterT, (TEST_CASE FILE_SUFFIX), {
+    auto hits = ExampleHitCollection();
+    auto hit = hits.create();
+    hit.x(1.23);
+    hit.y(2.34);
+    hit.z(3.45);
+    hit.energy(5.67);
+
+    auto clusters = ExampleClusterCollection();
+    auto cluster = clusters.create();
+    cluster.addoldHits(hit);
+
+    event.put(std::move(hits), "related_collection");
+    event.put(std::move(clusters), "evolution_collection");
+  })
+
+  READ_WITH(ReaderT, (TEST_CASE FILE_SUFFIX), {
+    const auto& hitColl = event.get<ExampleHitCollection>("related_collection");
+    ASSERT_EQUAL(hitColl.hasID(), true, "Readable collection should be available");
+    ASSERT_EQUAL(hitColl[0].x(), 1.23, "Readable elements should not change");
+    ASSERT_EQUAL(hitColl[0].y(), 2.34, "Readable elements should not change");
+    ASSERT_EQUAL(hitColl[0].z(), 3.45, "Readable elements should not change");
+    ASSERT_EQUAL(hitColl[0].energy(), 5.67, "Readable elements should not change");
+
+    const auto& clusterColl = event.get<ExampleClusterCollection>("evolution_collection");
+    ASSERT_EQUAL(clusterColl.hasID(), false, "Unreadable collection should be skipped");
+  })
+}
