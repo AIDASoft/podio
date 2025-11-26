9f9dd627b7091fc33b75796f874756396eb76edf
Thomas Madlener <thomas.madlener@desy.de>
2025-11-26 11:07:08 +0100

    Make test for renaming member variables more comprehensive

--- tests/schema_evolution/code_gen/datatypes_rename_member/check.cpp
+++ tests/schema_evolution/code_gen/datatypes_rename_member/check.cpp
@@ -3,7 +3,14 @@
 #include "check_base.h"
 
 int main() {
-  WRITE_AS(nsp::TestTypeCollection, { elem.oldEnergy(3.14); });
-  READ_AS(nsp::TestTypeCollection,
-          { ASSERT_EQUAL(elem.energy(), 3.14, "Renamed datatype member does not have the expected value"); })
+  WRITE_AS(nsp::TestTypeCollection, {
+    elem.oldEnergy(3.14);
+    elem.unchangedMember(123);
+    elem.anotherNonChange(4.3f);
+  });
+  READ_AS(nsp::TestTypeCollection, {
+    ASSERT_EQUAL(elem.energy(), 3.14, "Renamed datatype member does not have the expected value");
+    ASSERT_EQUAL(elem.unchangedMember(), 123, "Non renamed member should not change");
+    ASSERT_EQUAL(elem.anotherNonChange(), 4.3f, "Non renamed member should not change");
+  })
 }
