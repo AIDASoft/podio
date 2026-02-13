87571a8c64cf11d41d48c0d3e086be7c1024cb62
Thomas Madlener <thomas.madlener@desy.de>
2026-02-13 20:20:58 +0100

    Add format support for GenericParameters

--- src/GenericParameters.cc
+++ src/GenericParameters.cc
@@ -19,27 +19,34 @@ GenericParameters::GenericParameters(const GenericParameters& other) {
   _doubleMap = other._doubleMap;
 }
 
-template <typename MapType>
-void formatMap(const MapType& map, std::ostream& os) {
-  fmt::format_to(std::ostreambuf_iterator(os), "{:<30}{}\n{:-<80}", "Key", "Value", "");
-  for (const auto& [key, value] : map) {
-    fmt::format_to(std::ostreambuf_iterator(os), "{:<30}{}\n", key, value);
-  }
-}
-
 void GenericParameters::print(std::ostream& os, bool flush) const {
-  os << "int parameters\n\n";
-  formatMap(getMap<int>(), os);
-  os << "float parameters\n\n";
-  formatMap(getMap<float>(), os);
-  os << "double parameters\n\n";
-  formatMap(getMap<double>(), os);
-  os << "string parameters\n\n";
-  formatMap(getMap<std::string>(), os);
-
+  fmt::format_to(std::ostreambuf_iterator(os), "{}", *this);
   if (flush) {
     os.flush();
   }
 }
 
 } // namespace podio
+
+fmt::format_context::iterator fmt::formatter<podio::GenericParameters>::format(const podio::GenericParameters& params,
+                                                                               fmt::format_context& ctx) const {
+  auto out = ctx.out();
+
+  auto formatMap = [&out](const auto& map) {
+    out = fmt::format_to(out, "{:<30}{}\n{:-<80}\n", "Key", "Value", "");
+    for (const auto& [key, value] : map) {
+      out = fmt::format_to(out, "{:<30}{}\n", key, value);
+    }
+  };
+
+  out = fmt::format_to(out, "int parameters\n\n");
+  formatMap(params.getMap<int>());
+  out = fmt::format_to(out, "float parameters\n\n");
+  formatMap(params.getMap<float>());
+  out = fmt::format_to(out, "double parameters\n\n");
+  formatMap(params.getMap<double>());
+  out = fmt::format_to(out, "string parameters\n\n");
+  formatMap(params.getMap<std::string>());
+
+  return out;
+}
