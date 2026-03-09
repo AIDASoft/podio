3528b41d66f6adc3a37a1adc06ba1222904b8dce
Juan Miguel Carceller <22276694+jmcarcell@users.noreply.github.com>
2026-03-09 14:49:24 +0100

    Rename `CMAKE_{SOURCE,BIN}_DIR` to `PROJECT_{SOURCE,BIN}_DIR` (#942)
    Co-authored-by: Juan Miguel Carceller <jmcarcell@users.noreply.github.com>

--- cmake/podioDoxygen.cmake
+++ cmake/podioDoxygen.cmake
@@ -2,16 +2,16 @@ find_package(Doxygen)
 find_package(Sphinx)
 
 if(DOXYGEN_FOUND AND SPHINX_FOUND)
-  set(SPHINX_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/doc/_build)
+  set(SPHINX_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/doc/_build)
 
   add_custom_target(documentation
                     COMMAND
                     ${CMAKE_COMMAND} -E env
                     "ROOT_LIBRARY_PATH="
-                    "LD_LIBRARY_PATH=${CMAKE_BINARY_DIR}/src/:$ENV{LD_LIBRARY_PATH}"
-                    "ROOT_INCLUDE_PATH=${CMAKE_SOURCE_DIR}/include:$ENV{ROOT_INCLUDE_PATH}"
+                    "LD_LIBRARY_PATH=${PROJECT_BINARY_DIR}/src/:$ENV{LD_LIBRARY_PATH}"
+                    "ROOT_INCLUDE_PATH=${PROJECT_SOURCE_DIR}/include:$ENV{ROOT_INCLUDE_PATH}"
                     ${SPHINX_BUILD_EXECUTABLE} -M html
-                    ${CMAKE_SOURCE_DIR}/doc ${SPHINX_OUTPUT_DIRECTORY}
+                    ${PROJECT_SOURCE_DIR}/doc ${SPHINX_OUTPUT_DIRECTORY}
                     COMMENT "Building documentation" VERBATIM
                     DEPENDS podio::podio podio::podioRootIO
                     )
