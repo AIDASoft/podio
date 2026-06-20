a639044f81461b7446375a203392363f0c2dba6b
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-06-20 17:39:50 +0200

    Make the test better

--- tests/root_io/test_root_macro.C
+++ tests/root_io/test_root_macro.C
@@ -1,25 +1,42 @@
-#include "datamodel/TestLinkCollection.h"
-#include "datamodel/ExampleHitCollection.h"
 #include "datamodel/ExampleClusterCollection.h"
+#include "datamodel/ExampleHitCollection.h"
+#include "datamodel/TestLinkCollection.h"
+
+#include <iostream>
+#include <stdexcept>
 
 void test_root_macro() {
-    // Link collection
-    auto linkCol = TestLinkCollection{};
-    auto link = linkCol.create();
-    link.setWeight(0.5);
-    if (link.getWeight() != 0.5) exit(1);
-    if (linkCol.size() != 1) exit(1);
+  auto success = true;
+  const auto check = [&success](const char* name, const auto& actual, const auto& expected) {
+    if (actual == expected) {
+      return;
+    }
 
-    // Regular datatype collections
-    auto hitCol = ExampleHitCollection{};
-    auto hit = hitCol.create();
-    hit.energy(42.0);
-    if (hit.energy() != 42.0) exit(1);
-    if (hitCol.size() != 1) exit(1);
+    std::cerr << "test_root_macro: " << name << " failed: expected " << expected << ", got " << actual << '\n';
+    success = false;
+  };
 
-    auto clusterCol = ExampleClusterCollection{};
-    auto cluster = clusterCol.create();
-    cluster.energy(7.0);
-    if (cluster.energy() != 7.0) exit(1);
-    if (clusterCol.size() != 1) exit(1);
+  // Link collection
+  auto linkCol = TestLinkCollection{};
+  auto link = linkCol.create();
+  link.setWeight(0.5);
+  check("link weight", link.getWeight(), 0.5);
+  check("link collection size", linkCol.size(), 1u);
+
+  // Regular datatype collections
+  auto hitCol = ExampleHitCollection{};
+  auto hit = hitCol.create();
+  hit.energy(42.0);
+  check("hit energy", hit.energy(), 42.0);
+  check("hit collection size", hitCol.size(), 1u);
+
+  auto clusterCol = ExampleClusterCollection{};
+  auto cluster = clusterCol.create();
+  cluster.energy(7.0);
+  check("cluster energy", cluster.energy(), 7.0);
+  check("cluster collection size", clusterCol.size(), 1u);
+
+  if (!success) {
+    throw std::runtime_error("test_root_macro failed");
+  }
 }
